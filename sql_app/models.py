c4f8f2138eed7658e7d27ac2488f4965009354b9
praveen@samisen.io <praveen@samisen.io>
2023-10-04 12:29:43 +0530

    Implemented crud operations

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-from datetime import datetime
 
 from .database import Base
 
@@ -10,23 +9,18 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    conferences = relationship("Conference", back_populates="owner")
+    items = relationship("Item", back_populates="owner")
 
 
-class Conference(Base):
-    __tablename__ = "conferences"
+class Item(Base):
+    __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    start_date = Column(datetime, index=False)
-    start_date = Column(datetime, index=False)
-    end_date = Column(datetime, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="conferences")
+    owner = relationship("User", back_populates="items")
