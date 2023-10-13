eb362feb79bd48da9e6efbe30cabf87642663ecb
praveen1621 <praveen@samisen.io>
2023-10-13 14:39:28 +0530

    Resolved CORS error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import Depends, FastAPI, HTTPException
 from .routers import ai_models, users, conferences, ai_models,sessions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #TODO: Change this to the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(users.router)
