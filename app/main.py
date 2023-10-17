a21c5ceb059023f79b9de9620ade0562ab141c3c
praveen1621 <praveen@samisen.io>
2023-10-17 15:19:45 +0530

    CORS update

--- app/main.py
+++ app/main.py
@@ -2,22 +2,16 @@ from fastapi import Depends, FastAPI, HTTPException
 from .routers import ai_models, users, conferences, ai_models, sessions, settings
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-import os
 
 app = FastAPI()
 
-load_dotenv()
-
-CONFERENCE_PORTAL_LINKS = os.environ.get("CONFERENCE_PORTAL_LINKS").split(",")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CONFERENCE_PORTAL_LINKS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(users.router)
 app.include_router(conferences.router)
