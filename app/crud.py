9fbf488b7e6f228bef8dd492e96892eb37becd9e
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-10-18 10:47:16 +0530

    Praveen fix: Modified the conference model and fixed bugs(CA-31) (#11)
    * CORS Update Used ENV file to get domains
    
    * added settings table and separated APIs in Swagger Docs
    
    * Added Read with created_on and updated_on CRUD
    
    * (feat): Implemeted settings table
    
    Created a settings table to accommodate the settings for a conference
    
    * Code Refactor
    
    * CORS update
    
    * Removed all unnecessary imports
    
    * Code Refactor
    
    Checked all CRUD of conference model and made changes to fix all bugs
    
    * Changed endpoint names

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime,date
 from . import models, schemas
 import pytz
 
@@ -48,10 +48,11 @@ def get_users_by_account_type(db: Session, account_type: str):
 def get_users_by_bussiness_type(db: Session, bussiness_type: str):
     return db.query(models.User).filter(models.User.bussiness_type == bussiness_type).all()
 
-#crud for conference
+# get all conferences
 def get_conferences(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Conference).offset(skip).limit(limit).all()
 
+# create conference
 def create_user_conference(db: Session, conference: schemas.ConferenceCreate, user_id: int):
     db_conference = models.Conference(**conference.model_dump(), owner_id=user_id)
     db.add(db_conference)
@@ -59,57 +60,77 @@ def create_user_conference(db: Session, conference: schemas.ConferenceCreate, us
     db.refresh(db_conference)
     return db_conference
 
+# get conference by conference id
 def get_conference(db: Session, conference_id: int):
     return db.query(models.Conference).filter(models.Conference.id == conference_id).first()
 
+#get conference by owner id and conference id
+def get_conference_by_owner_id(db: Session, owner_id: int, conference_id: int):
+    return db.query(models.Conference).filter(models.Conference.owner_id == owner_id, models.Conference.id == conference_id).first()
+
+# get conference by name
 def get_conferences_by_name(db: Session, name: str):
     return db.query(models.Conference).filter(models.Conference.name == name).all()
 
+# get conference by location
 def get_conferences_by_location(db: Session, location: str):
     return db.query(models.Conference).filter(models.Conference.location == location).all()
 
-def get_conferences_by_start_date(db: Session, start_date: str):
+# get conference by start_date
+def get_conferences_by_start_date(db: Session, start_date: date):
     return db.query(models.Conference).filter(models.Conference.start_date == start_date).all()
 
+# get conference by end_date
 def get_conferences_by_end_date(db: Session, end_date: str):
     return db.query(models.Conference).filter(models.Conference.end_date == end_date).all()
 
+# get conference by description
 def get_conferences_by_description(db: Session, description: str):
     return db.query(models.Conference).filter(models.Conference.description == description).all()
 
-#get conferences by name by owner_id
-def get_conferences_by_name_owner_id(db: Session, name: str, owner_id: int):
+# get conferences by owner_id by name
+def get_conferences_owner_id_by_name(db: Session, name: str, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.name == name, models.Conference.owner_id == owner_id).all()
 
-#get conferences by location by owner_id
-def get_conferences_by_location_owner_id(db: Session, location: str, owner_id: int):
+# get conferences by owner_id by location
+def get_conferences_owner_id_by_location(db: Session, location: str, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.location == location, models.Conference.owner_id == owner_id).all()
 
-#get conferences by start_date by owner_id
-def get_conferences_by_start_date_owner_id(db: Session, start_date: str, owner_id: int):
+# get conferences by owner_id by start_date
+def get_conferences_owner_id_by_start_date(db: Session, start_date: str, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.start_date == start_date, models.Conference.owner_id == owner_id).all()
 
-#get conferences by end_date by owner_id
-def get_conferences_by_end_date_owner_id(db: Session, end_date: str, owner_id: int):
+# get conferences by owner_id by end_date
+def get_conferences_owner_id_by_end_date(db: Session, end_date: str, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.end_date == end_date, models.Conference.owner_id == owner_id).all()
 
-#get conferences by description by owner_id
-def get_conferences_by_description_owner_id(db: Session, description: str, owner_id: int):
+# get conferences by owner_id by description
+def get_conferences_owner_id_by_description(db: Session, description: str, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.description == description, models.Conference.owner_id == owner_id).all()
 
-#get conferences by owner_id
+# get conferences by owner_id
 def get_conferences_by_owner_id(db: Session, owner_id: int):
     return db.query(models.Conference).filter(models.Conference.owner_id == owner_id).all()
 
-#delete conference
-def delete_conference(db: Session, conference_id: int):
-    db.query(models.Conference).filter(models.Conference.id == conference_id).delete()
+# get all conferences by owner_id and conference_id and between start_date and end_date order by start_date
+def get_conferences_by_owner_id_between_start_date_and_end_date(db: Session, owner_id: int, start_date: date, end_date: date):
+    return db.query(models.Conference).filter(models.Conference.owner_id == owner_id, models.Conference.start_date >= start_date, models.Conference.end_date <= end_date).order_by(models.Conference.start_date).all()
+
+# delete conference by conference id
+def delete_conference(db: Session,owner_id: int, conference_id: int):
+    db.query(models.Conference).filter(models.Conference.id == conference_id, models.Conference.owner_id==owner_id).delete()
     db.commit()
     return True
 
-#update conference
-def update_conference(db: Session, conference: schemas.ConferenceCreate, conference_id: int):
-    db_conference = db.query(models.Conference).filter(models.Conference.id == conference_id).first()
+# delete all conferences by owner id
+def delete_all_conferences_of_owner_id(db: Session, owner_id: int):
+    db.query(models.Conference).filter(models.Conference.owner_id == owner_id).delete()
+    db.commit()
+    return True
+
+# update conference by owner id and conference id
+def update_conference(db: Session, conference: schemas.ConferenceCreate, owner_id: int, conference_id: int):
+    db_conference = db.query(models.Conference).filter(models.Conference.id == conference_id, models.Conference.owner_id == owner_id).first()
     db_conference.name = conference.name
     db_conference.location = conference.location
     db_conference.start_date = conference.start_date
@@ -193,6 +214,48 @@ def update_session(db: Session, session: schemas.SessionCreate, session_id: int)
     db.commit()
     db.refresh(db_session)
     return db_session
+
+#crud for settings
+def get_settings(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Settings).offset(skip).limit(limit).all()
+
+#get settings by id
+def get_settings_by_id(db: Session, settings_id: int):
+    return db.query(models.Settings).filter(models.Settings.id == settings_id).first()
+
+#create settings
+def create_settings(db: Session, body: dict, conference_id: int):
+    tz = pytz.timezone('Asia/Kolkata')
+    db_settings = models.Settings(body=body, conference_id=conference_id, created_on=datetime.now(tz), updated_on=datetime.now(tz))
+    db.add(db_settings)
+    db.commit()
+    db.refresh(db_settings)
+    return db_settings
+
+def get_settings_by_conference_id(db: Session, conference_id: int):
+    return db.query(models.Settings).filter(models.Settings.conference_id == conference_id).first()
+
+def get_settings_by_body(db: Session, body: str):
+    return db.query(models.Settings).filter(models.Settings.body == body).all()
+
+#get settings by body by conference_id
+def get_settings_by_body_conference_id(db: Session, body: str, conference_id: int):
+    return db.query(models.Settings).filter(models.Settings.body == body, models.Settings.conference_id == conference_id).all()
+
+#delete settings with conference id and settings id
+def delete_settings(db: Session, conference_id: int):
+    db.query(models.Settings).filter(models.Settings.conference_id == conference_id).delete()
+    db.commit()
+    return True
+
+#update settings
+def update_settings(db: Session, body: str, conference_id: int):
+    db_settings = db.query(models.Settings).filter(models.Settings.conference_id == conference_id).first()
+    db_settings.body = body
+    db_settings.updated_on = datetime.now(pytz.timezone('Asia/Kolkata'))
+    db.commit()
+    db.refresh(db_settings)
+    return db_settings
 
 #crud for settings
 def get_settings(db: Session, skip: int = 0, limit: int = 100):
--- app/routers/conferences.py
+++ app/routers/conferences.py
@@ -2,139 +2,207 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from .. import crud, schemas
 from ..dependencies import get_db
+from datetime import date
 
 router = APIRouter(tags=["conferences"])
 
-#conference crud
-@router.post("/conferences/", response_model=schemas.Conference)
-def create_conference_for_user(
-    user_id: int, conference: schemas.ConferenceCreate, db: Session = Depends(get_db)
-):
+# create conference
+@router.post("/conferences/{user_id}", response_model=schemas.Conference)
+def create_conference_for_user(user_id: int, conference: schemas.ConferenceCreate, db: Session = Depends(get_db)):
+    if user_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid user id")
+    if not crud.get_user(db, user_id=user_id):
+        raise HTTPException(status_code=404, detail="User not found")
+    if conference.start_date > conference.end_date:
+        raise HTTPException(status_code=400, detail="Invalid date range")
     return crud.create_user_conference(db=db, conference=conference, user_id=user_id)
 
+# get all conferences
 @router.get("/conferences/", response_model=list[schemas.Conference])
-def read_conferences(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_all_conferences(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    if skip < 0 or limit < 0:
+        raise HTTPException(status_code=400, detail="Invalid query parameters")
     conferences = crud.get_conferences(db, skip=skip, limit=limit)
     if conferences is None or len(conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return conferences
 
+# get conference by conference id
 @router.get("/conferences/{conference_id}", response_model=schemas.Conference)
-def read_conference(conference_id: int, db: Session = Depends(get_db)):
+def get_conference_by_conference_id(conference_id: int, db: Session = Depends(get_db)):
+    if conference_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference id")
     db_conference = crud.get_conference(db, conference_id=conference_id)
     if db_conference is None:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conference
 
+# get all conferences by conference name
 @router.get("/conferences/name/{name}", response_model=list[schemas.Conference])
-def read_conferences_by_name(name: str, db: Session = Depends(get_db)):
+def get_all_conferences_by_conference_name(name: str, db: Session = Depends(get_db)):
+    if name.isnumeric():
+        raise HTTPException(status_code=400, detail="Invalid conference name")
     db_conference = crud.get_conferences_by_name(db, name=name)
-    if db_conference is None:
+    if db_conference is None or len(db_conference) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conference
 
-#get conference by location
+# get all conferences by location
 @router.get("/conferences/location/{location}", response_model=list[schemas.Conference])
-def read_conferences_by_location(location: str, db: Session = Depends(get_db)):
+def get_all_conferences_by_location(location: str, db: Session = Depends(get_db)):
+    if location.isnumeric():
+        raise HTTPException(status_code=400, detail="Invalid conference name")
     db_conference = crud.get_conferences_by_location(db, location=location)
     if db_conference is None or len(db_conference) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conference
 
-#get conferences by start_date
+# get all conferences by start_date
 @router.get("/conferences/start_date/{start_date}", response_model=list[schemas.Conference])
-def read_conferences_by_start_date(start_date: str, db: Session = Depends(get_db)):
+def get_all_conferences_by_start_date(start_date: date, db: Session = Depends(get_db)):
     db_conferences = crud.get_conferences_by_start_date(db, start_date=start_date)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conferences by end_date
+# get all conferences by end_date
 @router.get("/conferences/end_date/{end_date}", response_model=list[schemas.Conference])
-def read_conferences_by_end_date(end_date: str, db: Session = Depends(get_db)):
+def get_all_conferences_by_end_date(end_date: date, db: Session = Depends(get_db)):
     db_conferences = crud.get_conferences_by_end_date(db, end_date=end_date)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conference by description
+# get all conferences by description
 @router.get("/conferences/description/{description}", response_model=list[schemas.Conference])
-def read_conferences_by_description(description: str, db: Session = Depends(get_db)):
+def get_all_conferences_by_description(description: str, db: Session = Depends(get_db)):
+    if description.isnumeric():
+        raise HTTPException(status_code=400, detail="Invalid conference name")
     db_conference = crud.get_conferences_by_description(db, description=description)
     if db_conference is None or len(db_conference) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conference
 
-#get conferences by name by owner_id
-@router.get("/conferences/name/{name}/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_name_owner_id(name: str, owner_id: int, db: Session = Depends(get_db)):
-    db_conferences = crud.get_conferences_by_name_owner_id(db, name=name, owner_id=owner_id)
+# get all conferences by owner_id by name
+@router.get("/conferences/owner/{owner_id}/name/{name}", response_model=list[schemas.Conference])
+def get_all_conferences_owner_id_by_name(owner_id: int, name: str, db: Session = Depends(get_db)):
+    if name.isnumeric() or owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference name or owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conferences = crud.get_conferences_owner_id_by_name(db, name=name, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conferences by location by owner_id
-@router.get("/conferences/location/{location}/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_location_owner_id(location: str, owner_id: int, db: Session = Depends(get_db)):
-    db_conferences = crud.get_conferences_by_location_owner_id(db, location=location, owner_id=owner_id)
+# get all conferences by owner_id by location
+@router.get("/conferences/owner/{owner_id}/location/{location}", response_model=list[schemas.Conference])
+def get_all_conferences_owner_id_by_location(owner_id: int, location: str, db: Session = Depends(get_db)):
+    if location.isnumeric() or owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference name or owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conferences = crud.get_conferences_owner_id_by_location(db, location=location, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conferences by start_date by owner_id
-@router.get("/conferences/start_date/{start_date}/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_start_date_owner_id(start_date: str, owner_id: int, db: Session = Depends(get_db)):
-    db_conferences = crud.get_conferences_by_start_date_owner_id(db, start_date=start_date, owner_id=owner_id)
+# get all conferences by owner_id by start_date
+@router.get("/conferences/owner/{owner_id}/start_date/{start_date}", response_model=list[schemas.Conference])
+def get_all_conferences_owner_id_by_start_date(owner_id: int, start_date: date, db: Session = Depends(get_db)):
+    if owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conferences = crud.get_conferences_owner_id_by_start_date(db, start_date=start_date, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conferences by end_date by owner_id
-@router.get("/conferences/end_date/{end_date}/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_end_date_owner_id(end_date: str, owner_id: int, db: Session = Depends(get_db)):
-    db_conferences = crud.get_conferences_by_end_date_owner_id(db, end_date=end_date, owner_id=owner_id)
+# get all conferences by owner_id by end_date
+@router.get("/conferences/owner/{owner_id}/end_date/{end_date}", response_model=list[schemas.Conference])
+def get_all_conferences_owner_id_by_end_date(owner_id: int, end_date: date, db: Session = Depends(get_db)):
+    if owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conferences = crud.get_conferences_owner_id_by_end_date(db, end_date=end_date, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get conferences by description by owner_id
-@router.get("/conferences/description/{description}/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_description_owner_id(description: str, owner_id: int, db: Session = Depends(get_db)):
-    db_conferences = crud.get_conferences_by_description_owner_id(db, description=description, owner_id=owner_id)
+# get all conferences by owner_id by description
+@router.get("/conferences/owner/{owner_id}/description/{description}", response_model=list[schemas.Conference])
+def get_all_conferences_owner_id_by_description(owner_id: int, description: str, db: Session = Depends(get_db)):
+    if description.isnumeric() or owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference name or owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conferences = crud.get_conferences_owner_id_by_description(db, description=description, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#get all conferences by owner_id
+# get all conferences by owner_id
 @router.get("/conferences/owner/{owner_id}", response_model=list[schemas.Conference])
-def read_conferences_by_owner_id(owner_id: int, db: Session = Depends(get_db)):
+def get_all_conferences_by_owner_id(owner_id: int, db: Session = Depends(get_db)):
+    if owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid owner id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
     db_conferences = crud.get_conferences_by_owner_id(db, owner_id=owner_id)
     if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
     return db_conferences
 
-#update conference
-@router.put("/conferences/{conference_id}", response_model=schemas.Conference)
-def update_conference(conference_id: int, conference: schemas.ConferenceCreate, db: Session = Depends(get_db)):
-    db_conference = crud.get_conference(db, conference_id=conference_id)
-    if db_conference is None:
+# get all conferences by owner_id and conference id and between start_date and end_date
+@router.get("/conferences/owner_id/{owner_id}/start_date/{start_date}/end_date/{end_date}", response_model=list[schemas.Conference])
+def get_all_conferences_by_owner_id_between_start_date_and_end_date(owner_id: int, start_date: date, end_date: date, db: Session = Depends(get_db)):
+    if owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid id")
+    if crud.get_user(db, user_id=owner_id) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if start_date > end_date:
+        raise HTTPException(status_code=400, detail="Invalid date range")
+    db_conferences = crud.get_conferences_by_owner_id_between_start_date_and_end_date(db, owner_id=owner_id, start_date=start_date, end_date=end_date)
+    if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="Conference not found")
-    return crud.update_conference(db=db, conference=conference, conference_id=conference_id)
+    return db_conferences
 
-#delete conference
-@router.delete("/conferences/{conference_id}")
-def delete_conference(conference_id: int, db: Session = Depends(get_db)):
-    db_conference = crud.get_conference(db, conference_id=conference_id)
+# update conference by owner id and conference id
+@router.put("/conferences/owner_id/{owner_id}/conference/{conference_id}", response_model=schemas.Conference)
+def update_conference(owner_id: int, conference_id: int, conference: schemas.ConferenceCreate, db: Session = Depends(get_db)):
+    if owner_id <= 0 or conference_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid id or conference id")
+    if not crud.get_user(db, user_id=owner_id):
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conference = crud.get_conference_by_owner_id(db, owner_id=owner_id, conference_id=conference_id)
     if db_conference is None:
         raise HTTPException(status_code=404, detail="Conference not found")
-    return crud.delete_conference(db=db, conference_id=conference_id)
+    if conference.start_date > conference.end_date:
+        raise HTTPException(status_code=400, detail="Invalid date range")
+    return crud.update_conference(db=db, owner_id=owner_id, conference=conference, conference_id=conference_id)
+
+# delete conference
+@router.delete("/conferences/owner_id/{owner_id}/conference/{conference_id}")
+def delete_conference_owner_id_conference_id(owner_id: int, conference_id: int, db: Session = Depends(get_db)):
+    if owner_id <= 0 or conference_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid id or conference id")
+    if not crud.get_user(db, user_id=owner_id):
+        raise HTTPException(status_code=404, detail="User not found")
+    db_conference = crud.get_conference_by_owner_id(db,owner_id=owner_id, conference_id=conference_id)
+    if db_conference is None:
+        raise HTTPException(status_code=404, detail="Conference not found")
+    return crud.delete_conference(db=db, owner_id=owner_id, conference_id=conference_id)
 
 # delete all conferences by owner id
 @router.delete("/conferences/owner/{owner_id}")
-def delete_conferences_by_owner(owner_id: int, db: Session = Depends(get_db)):
+def delete_all_conferences_by_owner_id(owner_id: int, db: Session = Depends(get_db)):
+    if owner_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid owner id")
+    if not crud.get_user(db, user_id=owner_id):
+        raise HTTPException(status_code=404, detail="User not found")
     db_conferences = crud.get_conferences_by_owner_id(db, owner_id=owner_id)
-    if not db_conferences or len(db_conferences) == 0:
+    if db_conferences is None or len(db_conferences) == 0:
         raise HTTPException(status_code=404, detail="No conferences found for owner")
-    for conference in db_conferences:
-        crud.delete_conference(db=db, conference_id=conference.id)
-    return {"message": "Conferences deleted successfully"}
+    return crud.delete_all_conferences_of_owner_id(db=db, owner_id=owner_id)
