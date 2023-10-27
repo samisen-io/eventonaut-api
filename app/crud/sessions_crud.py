613f71a46392e7868e3c794f1198ca1f78b8019f
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-10-27 13:29:17 +0530

    Praveen-user-authorization(app): Updated the Users, Conferences, Sessions, Settings to support authentication (#20)
    * Made User endpoints wokring with the user_id from the token
    
    * Modified User Model to support Authorization
    
    * Updated Users Model
    
    * Updated Conference Model, Conference CRUD to support authorization
    
    * Updated Sessions Model, Sessions CRUD to support authorization
    
    * Updated Settings Models, Settings Crud to support authentication

--- app/crud/sessions_crud.py
+++ app/crud/sessions_crud.py
@@ -9,9 +9,8 @@ from pytz import timezone
 def get_sessions(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Session).offset(skip).limit(limit).all()
 
-def create_conference_session(db: Session, session: schemas.SessionCreate, conference_id: int):
-    db_session = models.Session(**session.model_dump(), conference_id=conference_id)
-    owner_id=db.query(models.Conference).filter(models.Conference.id == conference_id).first().owner_id
+def create_conference_session(db: Session, session: schemas.SessionCreate, owner_id: int):
+    db_session = models.Session(**session.model_dump())
     tz = timezone('Asia/Kolkata')
     db_session.created_on = datetime.now(tz)
     db_session.updated_on = datetime.now(tz)
@@ -23,6 +22,10 @@ def create_conference_session(db: Session, session: schemas.SessionCreate, confe
 
 def get_session(db: Session, session_id: int):
     return db.query(models.Session).filter(models.Session.id == session_id).first()
+
+# get sessions by owner id and session id
+def get_session_by_owner_id(db: Session, session_id: int, owner_id: int):
+    return db.query(models.Session).filter(models.Session.id == session_id, models.Session.owner_id == owner_id).first()
 
 def get_sessions_by_name(db: Session, conference_id:int, name: str):
     return db.query(models.Session).filter(models.Session.conference_id == conference_id, models.Session.name == name).all()
@@ -52,12 +55,12 @@ def get_all_sessions_by_conference_id_between_date(db: Session, conference_id: i
     return db.query(models.Session).filter(models.Session.conference_id == conference_id, models.Session.date >= filter_start_date, models.Session.date <= filter_end_date).all()
 
 # get all conferences based on name or description strings
-def get_all_sessions_by_conference_id_and_name_or_description(db: Session, conference_id: int, search_string: str):
+def get_all_sessions_by_search(db: Session,conference_id:int, search_string: str):
     return db.query(models.Session).filter(models.Session.conference_id == conference_id, or_(models.Session.name.like('%'+search_string+'%'), models.Session.description.like('%'+search_string+'%'))).all()
 
 #delete session
-def delete_session(db: Session, session_id: int):
-    db.query(models.Session).filter(models.Session.id == session_id).delete()
+def delete_session(db: Session, session_id: int, owner_id: int):
+    db.query(models.Session).filter(models.Session.id == session_id,models.Session.owner_id == owner_id).delete()
     db.commit()
     return True
 
@@ -68,8 +71,8 @@ def delete_all_sessions_by_conference_id(db: Session, conference_id: int):
     return True
 
 #update session
-def update_session(db: Session, session: schemas.SessionCreate, session_id: int):
-    db_session = db.query(models.Session).filter(models.Session.id == session_id).first()
+def update_session(db: Session, session: schemas.SessionUpdate, session_id: int, owner_id: int):
+    db_session = db.query(models.Session).filter(models.Session.id == session_id,models.Session.owner_id == owner_id).first()
     tz = timezone('Asia/Kolkata')
     db_session.name = session.name
     db_session.date = session.date
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from app.oauth2 import get_current_active_user
 from ..schemas import user_schemas as schemas
 from ..crud import users_crud as crud
 from ..dependencies import get_db
 from email_validator import validate_email, EmailNotValidError
 
+
 router = APIRouter(tags=["users"])
 
-@router.post("/users/", response_model=schemas.User)
+
+@router.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # validate email
     try:
         valid = validate_email(user.email)
         user.email = valid.email
@@ -22,77 +22,56 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-@router.get("/users/", response_model=list[schemas.User])
-def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+@router.get("/users/all_users", response_model=list[schemas.User])
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     if users is None or len(users) == 0:
         raise HTTPException(status_code=404, detail="User not found")
     return users
 
-@router.get("/users/{user_id}", response_model=schemas.User)
-def get_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if user_id <= 0:
+@router.get("/users", response_model=schemas.User)
+def get_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 #update user by user id and check if email is already registered
-@router.put("/users/email/{user_id}", response_model=schemas.User)
-def update_user(user_id: int, user: schemas.UserBase, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if user_id <= 0:
+@router.put("/users", response_model=schemas.User)
+def update_user(user: schemas.UserBase, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    # validate email
     try:
         valid = validate_email(user.email)
         user.email = valid.email
     except EmailNotValidError as e:
         raise HTTPException(status_code=400, detail="Invalid email")
     db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user and db_user.id != user_id:
+    if db_user and db_user.id != current_user.id:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.update_user(db=db, user=user, user_id=user_id)
+    return crud.update_user(db=db, user=user, user_id=current_user.id)
 
 # upddate password by user id
-@router.put("/users/password/{user_id}", response_model=schemas.User)
-def update_user_password(user_id: int, user: schemas.UserPassword, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if user_id <= 0:
+@router.put("/users/password", response_model=schemas.User)
+def update_user_password(user: schemas.UserPassword, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return crud.update_user_password(db=db, user=user, user_id=user_id)
+    return crud.update_user_password(db=db, user=user, user_id=current_user.id)
 
 #delete user
-@router.delete("/users/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if user_id <= 0:
+@router.delete("/users")
+def delete_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return crud.delete_user(db=db, user_id=user_id)
-
-#get user by account_type
-@router.get("/users/account_type/{account_type}", response_model=list[schemas.User])
-def read_user_by_account_type(account_type: str, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if account_type.isnumeric():
-        raise HTTPException(status_code=400, detail="Invalid account type")
-    db_user = crud.get_users_by_account_type(db, account_type=account_type)
-    if db_user is None or len(db_user) == 0:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-#get user by bussiness_type
-@router.get("/users/bussiness_type/{bussiness_type}", response_model=list[schemas.User])
-def read_user_by_bussiness_type(bussiness_type: str, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    if bussiness_type.isnumeric():
-        raise HTTPException(status_code=400, detail="Invalid bussiness type")
-    db_user = crud.get_users_by_bussiness_type(db, bussiness_type=bussiness_type)
-    if db_user is None or len(db_user) == 0:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    return crud.delete_user(db=db, user_id=current_user.id)
