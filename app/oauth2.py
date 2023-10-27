d2ccb417130ea1cd405926111046aafb33d35dbe
praveen1621 <praveen@samisen.io>
2023-10-27 10:56:22 +0530

    Modified User Model to support Authorization

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,7 +5,6 @@ from app.schemas.user_schemas import User
 from .dependencies import get_db
 from .crud import users_crud
 from sqlalchemy.orm import Session
-from .config import create_config
 
 oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -22,5 +21,4 @@ def get_current_user(db: Session = Depends(get_db),data: str = Depends(oauth_2_s
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.is_active is False:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
-    create_config(current_user.id)
     return current_user
--- app/routers/users.py
+++ app/routers/users.py
@@ -4,7 +4,6 @@ from app.oauth2 import get_current_active_user
 from ..schemas import user_schemas as schemas
 from ..crud import users_crud as crud
 from ..dependencies import get_db
-from ..config import get_config
 from email_validator import validate_email, EmailNotValidError
 
 
@@ -32,8 +31,7 @@ def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), cu
 
 @router.get("/users", response_model=schemas.User)
 def get_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    _user_id=get_config().getint('creadentials', 'user_id')
-    db_user = crud.get_user(db, user_id=_user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -41,8 +39,7 @@ def get_user(db: Session = Depends(get_db), current_user: schemas.User = Depends
 #update user by user id and check if email is already registered
 @router.put("/users", response_model=schemas.User)
 def update_user(user: schemas.UserBase, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    _user_id=get_config().getint('creadentials', 'user_id')
-    db_user = crud.get_user(db, user_id=_user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     try:
@@ -51,28 +48,26 @@ def update_user(user: schemas.UserBase, db: Session = Depends(get_db), current_u
     except EmailNotValidError as e:
         raise HTTPException(status_code=400, detail="Invalid email")
     db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user and db_user.id != _user_id:
+    if db_user and db_user.id != current_user.id:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.update_user(db=db, user=user, user_id=_user_id)
+    return crud.update_user(db=db, user=user, user_id=current_user)
 
 # upddate password by user id
 @router.put("/users/password", response_model=schemas.User)
 def update_user_password(user: schemas.UserPassword, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    _user_id=get_config().getint('creadentials', 'user_id')
-    if _user_id <= 0:
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=_user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return crud.update_user_password(db=db, user=user, user_id=_user_id)
+    return crud.update_user_password(db=db, user=user, user_id=current_user.id)
 
 #delete user
 @router.delete("/users")
 def delete_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
-    _user_id=get_config().getint('creadentials', 'user_id')
-    if _user_id <= 0:
+    if current_user.id <= 0:
         raise HTTPException(status_code=400, detail="Invalid user id")
-    db_user = crud.get_user(db, user_id=_user_id)
+    db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return crud.delete_user(db=db, user_id=_user_id)
+    return crud.delete_user(db=db, user_id=current_user.id)
