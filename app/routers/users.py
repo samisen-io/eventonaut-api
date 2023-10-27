ef09583918d2e9caf11922b5dda5f7c680fa2002
praveen1621 <praveen@samisen.io>
2023-10-27 11:19:24 +0530

    Updated Users Model

--- app/routers/users.py
+++ app/routers/users.py
@@ -10,7 +10,7 @@ from email_validator import validate_email, EmailNotValidError
 router = APIRouter(tags=["users"])
 
 
-@router.post("/users/", response_model=schemas.User)
+@router.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     try:
         valid = validate_email(user.email)
@@ -31,6 +31,8 @@ def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), cu
 
 @router.get("/users", response_model=schemas.User)
 def get_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid user id")
     db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -39,6 +41,8 @@ def get_user(db: Session = Depends(get_db), current_user: schemas.User = Depends
 #update user by user id and check if email is already registered
 @router.put("/users", response_model=schemas.User)
 def update_user(user: schemas.UserBase, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_active_user)):
+    if current_user.id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid user id")
     db_user = crud.get_user(db, user_id=current_user.id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -50,7 +54,7 @@ def update_user(user: schemas.UserBase, db: Session = Depends(get_db), current_u
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user and db_user.id != current_user.id:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.update_user(db=db, user=user, user_id=current_user)
+    return crud.update_user(db=db, user=user, user_id=current_user.id)
 
 # upddate password by user id
 @router.put("/users/password", response_model=schemas.User)
