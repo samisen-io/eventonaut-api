ad4bb28a843541a450605f68c9b2a68a3065edd7
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-10-27 18:27:22 +0530

    Praveen-fix: Modified the sessions search filter (#23)
    * Made User endpoints wokring with the user_id from the token
    
    * Modified User Model to support Authorization
    
    * Updated Users Model
    
    * Updated Conference Model, Conference CRUD to support authorization
    
    * Updated Sessions Model, Sessions CRUD to support authorization
    
    * Updated Settings Models, Settings Crud to support authentication
    
    * Updated Session endpoint paths
    
    * Update sql_app.db
    
    * Updated Conference date range amd search sessions filter
    
    * Updated Sessions search
    
    * sessions update

--- app/routers/sessions.py
+++ app/routers/sessions.py
@@ -49,8 +49,8 @@ def get_sessions_by_conference_id(conference_id: int, db: Session = Depends(get_
 # get all conferences based to name or description of the session
 @router.get("/sessions/{conference_id}/search/{search}", response_model=list[schemas.Session])
 def get_sessions_by_search(conference_id: int, search: str, db: Session = Depends(get_db)):
-    if conference_id <= 0 or search.isnumeric():
-        raise HTTPException(status_code=400, detail="Invalid conference id or search")
+    if conference_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference id")
     if conferences_crud.get_conference(db, conference_id=conference_id) is None:
         raise HTTPException(status_code=404, detail="Conference not found")
     db_sessions = crud.get_all_sessions_by_search(db,conference_id=conference_id, search_string=search)
