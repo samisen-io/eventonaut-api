c4a018804bc60b8f09a85ca2bd5c3a9d110ebf7a
praveen1621 <praveen@samisen.io>
2023-10-27 13:20:51 +0530

    Updated Settings Models, Settings Crud to support authentication

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -10,63 +10,57 @@ from ..dependencies import get_db
 router = APIRouter(tags=["settings"])
 
 # create settings by conference id and take body as any valid JSON and convert it to string
-@router.post("/settings/{conference_id}", response_model=schemas.Settings)
-def create_settings(conference_id: int, body: dict, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
-    if conference_id <= 0:
+@router.post("/settings", response_model=schemas.Settings)
+def create_settings(settings:schemas.SettingsCreate, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
+    if settings.conference_id <= 0:
         raise HTTPException(status_code=400, detail="Invalid conference id")
-    if conferences_crud.get_conference(db, conference_id=conference_id) is None:
+    if conferences_crud.get_conference_by_owner_id(db, conference_id=settings.conference_id,owner_id=current_user.id) is None:
         raise HTTPException(status_code=404, detail="Conference not found")
-    db_settings = crud.get_settings_by_conference_id(db, conference_id=conference_id)
+    db_settings = crud.get_settings_by_conference_id(db, conference_id=settings.conference_id,owner_id=current_user.id)
     if db_settings:
         raise HTTPException(status_code=400, detail="Settings already exists")
-    if body is None or len(body) == 0:
+    if settings.body is None or len(settings.body) == 0:
         raise HTTPException(status_code=400, detail="Body is empty")
-    return crud.create_settings(db=db, body=body, conference_id=conference_id)
+    return crud.create_settings(db=db, settings=settings, owner_id=current_user.id)
 
 # get all settings
-@router.get("/settings/", response_model=list[schemas.Settings])
-def read_settings(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+@router.get("/settings/all_settings", response_model=list[schemas.Settings])
+def get_settings(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     settings = crud.get_settings(db, skip=skip, limit=limit)
     if settings is None or len(settings) == 0:
         raise HTTPException(status_code=404, detail="Settings not found")
     return settings
 
-# get settings by id
-@router.get("/settings/conference/{settings_id}", response_model=schemas.Settings)
-def read_settings_by_id(settings_id: int, db: Session = Depends(get_db)):
-    if settings_id <= 0:
-        raise HTTPException(status_code=400, detail="Invalid settings id")
-    settings = crud.get_settings_by_id(db, settings_id=settings_id)
-    if settings is None:
-        raise HTTPException(status_code=404, detail="Settings not found")
-    return settings
-
 # get settings by conference id
-@router.get("/settings/conference/{conference_id}", response_model=schemas.Settings)
-def read_settings_by_conference_id(conference_id: int, db: Session = Depends(get_db)):
+@router.get("/settings/{conference_id}", response_model=schemas.Settings)
+def get_settings_by_conference_id(conference_id: int, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
     if conference_id <= 0:
         raise HTTPException(status_code=400, detail="Invalid conference id")
-    settings = crud.get_settings_by_conference_id(db, conference_id=conference_id)
+    settings = crud.get_settings_by_conference_id(db, conference_id=conference_id,owner_id=current_user.id)
     if settings is None:
         raise HTTPException(status_code=404, detail="Settings not found")
     return settings
 
 # update settings by conference id and settings id
-@router.put("/settings/conference/{conference_id}", response_model=schemas.Settings)
-def update_settings(conference_id: int, body: dict, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
-    db_settings = crud.get_settings_by_conference_id(db, conference_id=conference_id)
+@router.put("/settings", response_model=schemas.Settings)
+def update_settings(settings: schemas.SettingsCreate,db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
+    if settings.conference_id <= 0:
+        raise HTTPException(status_code=400, detail="Invalid conference id")
+    if conferences_crud.get_conference_by_owner_id(db, conference_id=settings.conference_id,owner_id=current_user.id) is None:
+        raise HTTPException(status_code=404, detail="Conference not found")
+    db_settings = crud.get_settings_by_conference_id(db, conference_id=settings.conference_id,owner_id=current_user.id)
     if db_settings is None:
-        raise HTTPException(status_code=404, detail="No such conference exists")
-    if body is None or len(body) == 0:
+        raise HTTPException(status_code=404, detail="Settings not found")
+    if settings.body is None or len(settings.body) == 0:
         raise HTTPException(status_code=400, detail="Body is empty")
-    return crud.update_settings(db=db, body=body, conference_id=conference_id)
+    return crud.update_settings(db=db, settings=settings, owner_id=current_user.id)
 
 # delete settings by conference id and settings id
-@router.delete("/settings/conference/{conference_id}")
+@router.delete("/settings/{conference_id}")
 def delete_settings(conference_id: int, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
     if conference_id <= 0:
         raise HTTPException(status_code=400, detail="Invalid conference id")
-    db_settings = crud.get_settings_by_conference_id(db, conference_id=conference_id)
+    db_settings = crud.get_settings_by_conference_id(db, conference_id=conference_id,owner_id=current_user.id)
     if db_settings is None:
         raise HTTPException(status_code=404, detail="Settings not found")
-    return crud.delete_settings(db=db, conference_id=conference_id)
+    return crud.delete_settings(db=db, conference_id=conference_id, owner_id=current_user.id)
