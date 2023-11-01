5b5f82af9f8b8eeea94bd0cf89a11c78f94f7aaf
praveen1621 <praveen@samisen.io>
2023-11-01 19:15:31 +0530

    Updated Settings Model

--- app/schemas/settings_schemas.py
+++ app/schemas/settings_schemas.py
@@ -1,9 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from fastapi import HTTPException
 
 #pydantic model for settings
 class SettingsCreate(BaseModel):
     conference_id: int
     body: dict
+
+    @validator('conference_id')
+    def id_must_be_positive(cls, v):
+        if v <= 0:
+            raise HTTPException(status_code=400, detail="Invalid conference id")
+        return v
+    
+    @validator('body')
+    def body_must_not_be_empty(cls, v):
+        if v is None or len(v) == 0:
+            raise HTTPException(status_code=400, detail="Body is empty")
+        return v
 
 #pydantic model for settings
 class Settings(SettingsCreate):
