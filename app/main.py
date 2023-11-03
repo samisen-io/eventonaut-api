f1bf325e027974b7c54086f29a39991e155dbe02
praveen1621 <praveen@samisen.io>
2023-11-03 18:41:40 +0530

    Remove uuid_fill router and its related functions.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import Depends, FastAPI
 from app.oauth2 import get_current_active_user
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp
-from . import uuid_fill
 
 app = FastAPI()
 
@@ -20,7 +19,6 @@ app.include_router(conferences.router)
 app.include_router(sessions.router)
 app.include_router(settings.router)
 app.include_router(otp.router)
-app.include_router(uuid_fill.router)
 app.include_router(ai_models.router, dependencies=[Depends(get_current_active_user)])
 app.include_router(authentication.router)
 
