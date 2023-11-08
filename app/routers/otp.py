89def8a5af57833dae30e84b9a5198ad4e20f270
praveen1621 <praveen@samisen.io>
2023-11-08 17:26:03 +0530

    Refactor OTP generator and verification

--- app/routers/otp.py
+++ app/routers/otp.py
@@ -2,20 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 from ..dependencies import get_db
 from ..crud import users_crud as crud
 from email_validator import validate_email, EmailNotValidError
-from ..otp_generator import send_mail, otp_generator
+from ..otp_generator import send_mail, generate_otp, validate_otp
 from sqlalchemy.orm import Session
 from datetime import datetime
 from ..crud import users_crud as crud
 
 router = APIRouter(tags=['OTP'])
-otp_gen: otp_generator
-valid_otp: bool = False
-valid_email: str = ""
+otp_db = dict()
 
 @router.post('/otp')
 async def send_otp(email: str, email_subject: str , db: Session = Depends(get_db)):
-    global valid_email, otp_gen
-    otp_gen = otp_generator()
+    global otp_db
     try:
         valid = validate_email(email)
         email = valid.email
@@ -24,31 +21,31 @@ async def send_otp(email: str, email_subject: str , db: Session = Depends(get_db
     user = crud.get_user_by_email(db, email)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    send_mail(otp_gen, email_subject, email)
-    valid_email = email
+    otp = generate_otp()
+    send_mail(otp, email_subject, email)
+    otp_db[email] = [otp, datetime.now(),False]
     return {"msg": "OTP sent successfully"}
 
 @router.post('/otp/verify')
 async def verify_otp(email: str, otp: str):
-    global valid_otp, valid_email, otp_gen
-    if email != valid_email:
+    global otp_db
+    if email not in otp_db.keys():
         raise HTTPException(status_code=400, detail="Email not verified")
     if len(otp) != 6:
         raise HTTPException(status_code=400, detail="Invalid OTP")
-    valid_otp = otp_gen.validate_otp(otp, datetime.now())
+    valid_otp = validate_otp(otp_db[email][0], otp, otp_db[email][1], datetime.now())
+    otp_db[email][2] = valid_otp
     if not valid_otp:
         raise HTTPException(status_code=400, detail="Invalid OTP or OTP expired")
     return {"msg": "OTP verified successfully"}
 
 @router.put('/otp/passwordreset')
 async def password_reset(email: str, password: str, db: Session = Depends(get_db)):
-    global valid_otp, valid_email, otp_gen
-    if valid_otp and email == valid_email:
+    global otp_db
+    if email in otp_db.keys() and otp_db[email][2]:
         user = crud.get_user_by_email(db, email)
         crud.update_user_password_by_id(db, user_id=user.id, password=password)
-        valid_otp = False
-        valid_email = ""
-        otp_gen = None
+        del otp_db[email]
         return {"msg": "Password updated successfully"}
     else:
         raise HTTPException(status_code=400, detail="OTP not verified")
