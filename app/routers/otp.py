e348e7d627cdda55049ee11bb669acd206cf9fc2
praveen1621 <praveen@samisen.io>
2023-11-10 11:28:38 +0530

    Fix OTP verification logic

--- app/routers/otp.py
+++ app/routers/otp.py
@@ -50,8 +50,9 @@ async def verify_otp(email: str, otp: str):
     if len(otp) != 6:
         raise HTTPException(status_code=400, detail="Invalid OTP")
     valid_otp = validate_otp(otp_db[email][0], otp, otp_db[email][1], datetime.now())
-    otp_db[email][2] = valid_otp
-    otp_db[email][0] = 0
+    if valid_otp:
+        otp_db[email][2] = valid_otp
+        otp_db[email][0] = 0
     if not valid_otp:
         raise HTTPException(status_code=400, detail="Invalid OTP or OTP expired")
     return {"msg": "OTP verified successfully"}
