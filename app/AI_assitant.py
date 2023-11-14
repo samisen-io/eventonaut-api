2dd8f581c3b7a591a27a41e9b55831a6cc128559
praveen1621 <praveen@samisen.io>
2023-11-14 13:07:29 +0530

    Add file upload functionality with supported file formats

--- app/AI_assitant.py
+++ app/AI_assitant.py
@@ -1,4 +1,7 @@
 from openai import OpenAI
+import os
+import logging
+from fastapi import UploadFile, HTTPException
 
 client = OpenAI()
 
@@ -12,8 +15,33 @@ def create_assistant(name: str):
     return assistant
 
 
-def file_upload(file):
+def file_upload(file: UploadFile):
     global client
+
+    app_folder = 'app'
+
+    files_folder = os.path.join(app_folder, 'files')
+
+    if not os.path.exists(files_folder):
+        os.makedirs(files_folder)
+    
+    if file.filename.endswith(".csv") or file.filename.endswith(".json") or file.filename.endswith(".xlsx"):
+        if file.filename.endswith(".csv"):
+            file_path = os.path.join(files_folder, 'sessions.csv')
+        elif file.filename.endswith(".json"):
+            file_path = os.path.join(files_folder, 'sessions.json')
+        elif file.filename.endswith(".xlsx"):
+            file_path = os.path.join(files_folder, 'sessions.xlsx')
+
+        logging.info("Uploading file to %s" % file_path)
+        
+        with open(file_path, "wb") as f:
+            f.write(file.file.read())
+
+        logging.info("File uploaded successfully")
+    else:
+        raise HTTPException(status_code=400, detail="File format not supported")
+
     uploaded_file = client.files.create(file=file)
     return uploaded_file
 
