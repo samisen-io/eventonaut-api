6a8ea60c021505ca60ff870e54ff91ab109f2125
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-11-16 18:17:34 +0530

    Fix conference code comparison in (#46)
    get_conference_by_code function and remove
    unnecessary uppercasing in generate_qr_code

--- app/routers/conferences.py
+++ app/routers/conferences.py
@@ -98,7 +98,7 @@ def generate_qr_code(conference_id: str, db: Session = Depends(get_db), current_
     qr_data = {
         "conference_id": conference.uuid,
         "conference_name": conference.name,
-        "conference_code": conference.code.upper()
+        "conference_code": conference.code
     }
 
     qr.add_data(json.dumps(qr_data))
