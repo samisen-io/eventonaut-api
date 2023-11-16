ecd0df214e01600d2abf3a8959655dcfc0e29515
praveen1621 <praveen@samisen.io>
2023-11-16 10:46:25 +0530

    Refactor attendee router endpoints

--- app/routers/attendee.py
+++ app/routers/attendee.py
@@ -8,7 +8,7 @@ from email_validator import validate_email, EmailNotValidError
 router = APIRouter(tags=["attendee"])
 
 # create attendee
-@router.post("/attendee/", response_model=schemas.Attendee)
+@router.post("/attendee", response_model=schemas.Attendee)
 def create_attendee(attendee: schemas.AttendeeCreate, db: Session = Depends(get_db)):
     try:
         valid = validate_email(attendee.email)
@@ -21,7 +21,7 @@ def create_attendee(attendee: schemas.AttendeeCreate, db: Session = Depends(get_
     return crud.create_attendee(db=db, attendee=attendee)
 
 # get all attendees
-@router.get("/attendee/", response_model=list[schemas.Attendee])
+@router.get("/attendee", response_model=list[schemas.Attendee])
 def get_all_attendees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     attendees = crud.get_attendees(db, skip=skip, limit=limit)
     if not attendees or len(attendees) == 0:
@@ -70,7 +70,7 @@ def delete_attendee_by_id(attendee_id: str, db: Session = Depends(get_db)):
     return crud.delete_attendee_by_uuid(db, attendee_id=attendee_id)
 
 # create attendee conference
-@router.post("/attendee/conference/", response_model=attendee_conference_schemas.AttendeeConference)
+@router.post("/attendee/conference", response_model=attendee_conference_schemas.AttendeeConference)
 def create_attendee_conference(attendee_conference: attendee_conference_schemas.AttendeeConferenceCreate, db: Session = Depends(get_db)):
     if not crud.get_attendee_by_uuid(db, attendee_id=attendee_conference.attendee_id):
         raise HTTPException(status_code=400, detail="Attendee not found")
