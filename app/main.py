37995fbaf6ff4b93cc709581c14b055f0b2d73a9
praveen1621 <praveen@samisen.io>
2023-11-16 12:42:48 +0530

    Add authentication middleware to assistant router

--- app/main.py
+++ app/main.py
@@ -15,13 +15,13 @@ app.add_middleware(
 )
 
 # Add the routers to the application with authentication middleware
-app.include_router(assistant.router)
 app.include_router(users.router)
 app.include_router(conferences.router)
 app.include_router(sessions.router)
 app.include_router(settings.router)
 app.include_router(attendee.router)
 app.include_router(agenda.router)
+app.include_router(assistant.router)
 app.include_router(otp.router)
 app.include_router(ai_models.router, dependencies=[Depends(get_current_active_user)])
 app.include_router(authentication.router)
