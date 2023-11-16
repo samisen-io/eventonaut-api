bf56ef611ad1183f3aef93bcf470ab74abb2ceed
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2023-11-16 11:25:01 +0530

    Feat ca 38 refresh token (security) (#44)
    * refresh token created
    
    * refresh token endpoint generated
    
    * refresh token mechanism implemented
    
    * now for token input schema is used
    
    * username correction

--- app/schemas/token_schemas.py
+++ app/schemas/token_schemas.py
@@ -3,6 +3,10 @@ from pydantic import BaseModel
 class Token(BaseModel):
     access_token: str
     token_type: str
+    refresh_token: str
     
 class TokenData(BaseModel):
     username: str or None = None
+    
+class TokenInput(BaseModel):
+    token: str
