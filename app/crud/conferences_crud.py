4b4030a008d8412663016b04f37c37791018fe04
praveen1621 <praveen@samisen.io>
2023-11-21 17:41:40 +0530

    Fix condition for deleting assistant in delete_conference function

--- app/crud/conferences_crud.py
+++ app/crud/conferences_crud.py
@@ -75,7 +75,7 @@ def delete_conference(db: Session, owner_id: int, uuid: str):
         db.delete(session)
     db.query(models.Settings).filter(models.Settings.conference_id == conference.id, models.Settings.owner_id == owner_id).delete()
     agenda_crud.delete_agenda_by_conference_id(db, conference_id=conference.id)
-    if conference.assistant_id is not None or conference.assistant_id != "None":
+    if conference.assistant_id is not None and conference.assistant_id != "None":
         AI_assitant.delete_assistant(assistant_id=conference.assistant_id)
     db.delete(conference)
     db.commit()
