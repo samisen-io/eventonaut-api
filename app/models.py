22e806419f74b66e6b8ba0dcfe87e0b48e0c1744
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-11-21 12:28:44 +0530

    Attendee feat: Added Profile image to the attendee (#48)
    * Fix assistant deletion in conferences_crud.py
    
    * Add profile image URL column to Attendee model
    
    * Add profile_image_url field to AttendeeBase and
    AttendeeUpdate schemas
    
    * Fix profile image URL handling in attendee CRUD

--- app/models.py
+++ app/models.py
@@ -113,6 +113,7 @@ class Attendee(Base):
     hashed_password = Column(String, index=True)
     is_active = Column(Boolean, default=True)
     thread_id = Column(String, index=True, default="None")
+    profile_image_url = Column(String, index=True, default="None")
 
     agenda = relationship("Agenda", back_populates="attendees")
     agenda_session = relationship("AgendaSession", back_populates="attendees")
