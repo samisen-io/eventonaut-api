4df100935aa7f7c01e4ebfd30bc23be170c0898a
praveen1621 <praveen@samisen.io>
2023-11-28 12:06:57 +0530

    Remove unused import statement

--- app/crud/agenda_crud.py
+++ app/crud/agenda_crud.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import Session
-from pytz import timezone
 from .. import models
 from ..schemas import agenda_schemas as schemas
 from . import conferences_crud, attendee_crud, sessions_crud
