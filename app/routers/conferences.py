86f6a66c3d9082d17ae3861855ebf696721ff3c0
praveen1621 <praveen@samisen.io>
2023-11-28 11:54:13 +0530

    Fix validation in update_conference function

--- app/routers/conferences.py
+++ app/routers/conferences.py
@@ -60,7 +60,7 @@ def get_all_conferences_by_owner_id(db: Session = Depends(get_db),current_user: 
 # update conference by conference id
 @router.put("/conferences", response_model=schemas.Conference)
 def update_conference(conference: schemas.ConferenceUpdate, db: Session = Depends(get_db), current_user: uschemas.User = Depends(get_current_active_user)):
-    if conference.name is None and conference.location is None and conference.start_date is None and conference.end_date is None and conference.description is None:
+    if all(value is None for value in dict(conference).values()):
         raise HTTPException(status_code=400, detail="Invalid request body")
     db_conference = crud.get_conference_by_uuid(db, uuid=conference.id, owner_id=current_user.id)
     if db_conference is None:
