862ab53848f404dc7a74b461c911cc37cefdb0f3
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2023-11-29 18:30:08 +0530

    Remove unnecessary code and comments

--- app/routers/ai_models.py
+++ app/routers/ai_models.py
@@ -30,7 +30,6 @@ async def query_document_endpoint(question: str, conference_id: str, db: Session
     file_ids = conferences_crud.get_file_ids_by_conference_id(db, conference_id)
     if not file_ids:
         raise HTTPException(status_code=404, detail="No files found for this conference")
-    print(assistant_id,thread_id, file_ids)
     answer = query_document(question,assistant_id,thread_id, file_ids)
     return {"answer": answer}
 
@@ -50,7 +49,7 @@ async def delete_file_from_openai(conference_id: str, current_user: User = Secur
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     conferences_crud.delete_file_id(db=db, file_id=file_id, conference_id=conference_id, owner_id=current_user.id)
-    assistant_schema = create_assistant_schema(assistant_id, conference_id, file_id=None)
+    assistant_schema = create_assistant_schema(assistant_id, conference_id, file_id="")
     update_assistant(assistant_schema)
     # delete the file from the files folder
     file_path = os.path.join('app', 'files')
@@ -62,9 +61,6 @@ async def delete_file_from_openai(conference_id: str, current_user: User = Secur
 async def update_conference(conference_id: str, current_user: User = Security(get_current_active_user, scopes=["organizer"]), db: Session = Depends(get_db)):
     write_data_to_json(conference_id,db)
     file_ids = conferences_crud.get_file_ids_by_conference_id(db, conference_id)
-    # if not file_ids:
-    #     raise HTTPException(status_code=404, detail="No files found for this conference")
-    # file_id = file_ids[0]
     if file_ids:
         file_id = file_ids[0]
         # delete from openai assistant api
@@ -73,28 +69,8 @@ async def update_conference(conference_id: str, current_user: User = Security(ge
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
         conferences_crud.delete_file_id(db=db, file_id=file_id, conference_id=conference_id, owner_id=current_user.id)
-    # upload it into openai assistant api
-    # new
     filename = await upload_session_from_database(conference_id, current_user, db)
     return filename
-    # file_path = os.path.join('app', 'files')
-    # file_path = os.path.join(file_path, 'sessions_'+str(conference_id)+'.json')
-    # # with open(file_path, 'rb') as file:
-    # try:
-    #     file = upload_file(file_path=file_path)
-    # except Exception as e:
-    #     raise HTTPException(status_code=400, detail=str(e))
-    # try:
-    #     conferences_crud.upload_file_id(db=db, file_id=file.id, conference_id=conference_id, owner_id=current_user.id)
-    # except Exception as e:
-    #     raise HTTPException(status_code=400, detail=f"Error uploading file: {str(e)}")
-    # try:
-    #     assistant_id = conferences_crud.get_assistant_id_by_conference_id(db, conference_id)
-    # except Exception as e:
-    #     raise HTTPException(status_code=400, detail=f"Error getting assistant ID: {str(e)}")
-    # assistant_schema = create_assistant_schema(assistant_id, conference_id, file.id)
-    # update_assistant(assistant_schema)
-    # return {"success": "Sessions file updated successfully."}
 
 @router.post("/upload_session_file/")
 async def upload_session_file(file: UploadFile,
@@ -139,7 +115,7 @@ async def upload_session_file(file: UploadFile,
     filename = await upload_session_from_database(conference_id, current_user, db)
     return filename
 
-@router.post("/upload_sessions_from_database/")
+# @router.post("/upload_sessions_from_database/")
 async def upload_session_from_database(conference_id: str,
                                        current_user: User = Security(get_current_active_user, scopes=["organizer"]),
                                        db: Session = Depends(get_db)):
