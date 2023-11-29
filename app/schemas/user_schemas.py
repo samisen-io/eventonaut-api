d4c993140808410003098af0a2b1a5bc12545f18
praveen1621 <praveen@samisen.io>
2023-11-29 17:06:48 +0530

    Fix user update bug

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -52,20 +52,11 @@ class UserBase(BaseModel):
         return v
     
 class UserBaseUpdate(BaseModel):
-    email: str | None = None
     first_name: str | None = None
     last_name: str | None = None
     company: str |None = None
     bussiness_type: str |None = None
     timezone: str |None = None
-    
-    @validator('email')
-    def email_is_valid(cls, v):
-        if v is not None and (v.strip() == "" or v == "string" or v.__contains__(" ")):
-            raise HTTPException(status_code=400, detail="Invalid email")
-        elif len(v) > 256:
-            raise HTTPException(status_code=400, detail="Email too long")
-        return v
 
     @validator('first_name')
     def first_name_is_not_empty(cls, v):
