477f6a894ef006957c2c7492e9fcff2eb6e3859d
praveen1621 <praveen@samisen.io>
2023-12-04 11:12:12 +0530

    Add logging and update attendee conference endpoints

--- app/routers/attendee_conference.py
+++ app/routers/attendee_conference.py
@@ -1,6 +1,6 @@
 # create attendee conference
 from fastapi import APIRouter, HTTPException, Depends, Security
-
+import logging
 from app.oauth2 import get_current_active_user
 from ..dependencies import get_db
 from sqlalchemy.orm import Session
@@ -19,7 +19,9 @@ def create_attendee_conference(attendee_conference: attendee_conference_schemas.
         raise HTTPException(status_code=400, detail="Conference not found")
     if crud.get_attendee_conference_by_attendee_id_and_conference_id(db=db, attendee_id=attendee_conference.attendee_id, conference_id=conference.uuid):
         raise HTTPException(status_code=400, detail="Conference already exists")
-    return crud.create_attendee_conference(db=db, attendee_conference=attendee_conference)
+    attendee_conf = crud.create_attendee_conference(db=db, attendee_conference=attendee_conference)
+    logging.info("Attendee conference created: " + attendee_conference.attendee_id)
+    return attendee_conf
 
 # get all attendee conferences
 @router.get("/attendee/conference/{attendee_id}", response_model=list[conference_schemas.Conference])
@@ -29,6 +31,7 @@ def get_all_attendee_conferences(attendee_id: str, skip: int = 0, limit: int = 1
     attendee_conferences = crud.get_all_attendee_conferences(db, skip=skip, limit=limit, attendee_id=attendee_id)
     if not attendee_conferences or len(attendee_conferences) == 0:
         raise HTTPException(status_code=404, detail="No conferences found")
+    logging.info("All attendee conferences retrieved")
     return attendee_conferences
 
 # delete attendee conference by attendee id and conference id
@@ -41,10 +44,13 @@ def delete_attendee_conference_by_attendee_id_and_conference_id(attendee_id: str
         raise HTTPException(status_code=400, detail="Conference not found")
     if not crud.get_attendee_conference_by_attendee_id_and_conference_id(db=db, attendee_id=attendee_id, conference_id=conference.uuid):
         raise HTTPException(status_code=404, detail="No conference found")
-    return crud.delete_attendee_conference_by_attendee_id_and_conference_id(db=db, attendee_id=attendee_id, conference_code=conference_code)
+    deleted_attendee_conference = crud.delete_attendee_conference_by_attendee_id_and_conference_id(db=db, attendee_id=attendee_id, conference_code=conference_code)
+    logging.info("Attendee conference deleted: " + attendee_id)
+    return deleted_attendee_conference
 
 @router.get("/attendee/profiles/", response_model=list[schemas.Attendee])
 def get_attendee_profiles_for_session_id(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["attendee"])):
     if not conferences_crud.get_conference_by_conference_uuid(db, uuid=conference_id):
         raise HTTPException(status_code=400, detail="Conference not found")
-    return crud.get_all_attendee_profiles_by_conference_id(db=db, conference_id=conference_id)
+    attendees = crud.get_all_attendee_profiles_by_conference_id(db=db, conference_id=conference_id)
+    return attendees
