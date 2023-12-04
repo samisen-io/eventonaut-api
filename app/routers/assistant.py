c599a9dbffae54a94f241f8969ef7c2076719f69
praveen1621 <praveen@samisen.io>
2023-12-04 11:12:28 +0530

    Update assistant endpoints

--- app/routers/assistant.py
+++ app/routers/assistant.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
-from app.oauth2 import get_current_active_user
-from .. import AI_assitant as assistant
+import logging
+from .. import AI_assitant
 from ..schemas import ai_assistant_schemas as schemas
 from .. import basicauth
 import re
@@ -10,54 +10,69 @@ router = APIRouter(tags=["AI_assistant"])
 @router.post("/assistant/create_assistant")
 def create_assistant(schema: schemas.AssistantCreate, basic_auth = Depends(basicauth.basic_auth)):
     try:
-        return assistant.create_assistant(schema=schema)
+        assistant = AI_assitant.create_assistant(schema=schema)
+        logging.info("Assistant created: " + schema.name)
+        return assistant
     except Exception as e:
         exception = str(e)
         hyphen_index = exception.find('-')
         error_code = int(re.search(r'\d+',exception[:hyphen_index].strip()).group())
         error_message = exception[hyphen_index+1:].strip()
+        logging.error(error_message)
         raise HTTPException(status_code=error_code, detail=error_message)
     
 @router.get("/assistant/get_assistant/{assistant_id}")
 def get_assistant(assistant_id: str, basic_auth = Depends(basicauth.basic_auth)):
     try:
-        return assistant.get_assistant(assistant_id=assistant_id)
+        assistant = AI_assitant.get_assistant(assistant_id=assistant_id)
+        logging.info("Assistant retrieved: " + assistant.name)
+        return assistant
     except Exception as e:
         exception = str(e)
         hyphen_index = exception.find('-')
         error_code = int(re.search(r'\d+',exception[:hyphen_index].strip()).group())
         error_message = exception[hyphen_index+1:].strip()
+        logging.error(error_message)
         raise HTTPException(status_code=error_code, detail=error_message)
     
 @router.put("/assistant/update_assistant")
 def update_assistant(schema: schemas.AssistantUpdate, basic_auth = Depends(basicauth.basic_auth)):
     try:
-        return assistant.update_assistant(schema=schema)
+        updated_assistant = AI_assitant.update_assistant(schema=schema)
+        logging.info("Assistant updated: " + schema.name)
+        return updated_assistant
     except Exception as e:
         exception = str(e)
         hyphen_index = exception.find('-')
         error_code = int(re.search(r'\d+',exception[:hyphen_index].strip()).group())
         error_message = exception[hyphen_index+1:].strip()
+        logging.error(error_message)
         raise HTTPException(status_code=error_code, detail=error_message)
     
 @router.delete("/assistant/delete_assistant/{assistant_id}")
 def delete_assistant(assistant_id: str, basic_auth = Depends(basicauth.basic_auth)):
     try:
-        return assistant.delete_assistant(assistant_id=assistant_id)
+        delete_assistant = AI_assitant.delete_assistant(assistant_id=assistant_id)
+        logging.info("Assistant deleted: " + assistant_id)
+        return delete_assistant
     except Exception as e:
         exception = str(e)
         hyphen_index = exception.find('-')
         error_code = int(re.search(r'\d+',exception[:hyphen_index].strip()).group())
         error_message = exception[hyphen_index+1:].strip()
+        logging.error(error_message)
         raise HTTPException(status_code=error_code, detail=error_message)
 
 @router.get("/assistant/list_assistants")
 def list_assistants(basic_auth = Depends(basicauth.basic_auth)):
     try:
-        return assistant.list_assistants()
+        assistant_list = AI_assitant.list_assistants()
+        logging.info("Assistants retrieved")
+        return assistant_list
     except Exception as e:
         exception = str(e)
         hyphen_index = exception.find('-')
         error_code = int(re.search(r'\d+',exception[:hyphen_index].strip()).group())
         error_message = exception[hyphen_index+1:].strip()
+        logging.error(error_message)
         raise HTTPException(status_code=error_code, detail=error_message)
