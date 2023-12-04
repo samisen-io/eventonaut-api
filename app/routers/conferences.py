7eeb6097c6a1985c4a2985fde198b3bf9260d3e3
praveen1621 <praveen@samisen.io>
2023-12-04 15:32:55 +0530

    Add logging statements for error handling in conference endpoints

--- app/routers/conferences.py
+++ app/routers/conferences.py
@@ -20,10 +20,13 @@ router = APIRouter(tags=["conferences"])
 @router.post("/conferences", response_model=schemas.Conference)
 def create_conference_for_user(conference: schemas.ConferenceCreate, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
     if current_user.id <= 0:
+        logging.exception("Invalid user id")
         raise HTTPException(status_code=400, detail="Invalid user id")
     if not users_crud.get_user(db, user_id=current_user.id):
+        logging.exception("User not found")
         raise HTTPException(status_code=404, detail="User not found")
     if conference.start_date > conference.end_date or conference.start_date < date.today():
+        logging.exception("Invalid date range")
         raise HTTPException(status_code=400, detail="Invalid date range")
     logging.info("Conference created: " + conference.name)
     return crud.create_user_conference(db=db, conference=conference, user_id=current_user.id)
@@ -32,9 +35,11 @@ def create_conference_for_user(conference: schemas.ConferenceCreate, db: Session
 @router.get("/conferences/all_conferences", response_model=list[schemas.Conference])
 def get_all_conferences(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     if offset < 0 or limit < 0:
+        logging.exception("Invalid query parameters")
         raise HTTPException(status_code=400, detail="Invalid query parameters")
     conferences = crud.get_all_conferences(db, offset=offset, limit=limit)
     if conferences is None or len(conferences) == 0:
+        logging.exception("No conferences found")
         raise HTTPException(status_code=404, detail="Conference not found")
     logging.info("Conferences retrieved")
     return conferences
@@ -43,9 +48,11 @@ def get_all_conferences(offset: int = 0, limit: int = 100, db: Session = Depends
 @router.get("/conferences/for_attendee", response_model=list[schemas.Conference])
 def get_all_conferences_for_attendee(offset: int = 0, limit: int = 100, db: Session = Depends(get_db),basic_auth = Depends(basicauth.basic_auth)):
     if offset < 0 or limit < 0:
+        logging.exception("Invalid query parameters")
         raise HTTPException(status_code=400, detail="Invalid query parameters")
     conferences = crud.get_all_conferences_for_attendee(db, offset=offset, limit=limit)
     if conferences is None or len(conferences) == 0:
+        logging.exception("No conferences found")
         raise HTTPException(status_code=404, detail="Conference not found")
     logging.info("Conferences retrieved for attendee")
     return conferences
@@ -54,25 +61,31 @@ def get_all_conferences_for_attendee(offset: int = 0, limit: int = 100, db: Sess
 @router.get("/conferences", response_model=list[schemas.Conference])
 def get_all_conferences_by_owner_id(db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
     if current_user.id <= 0:
+        logging.exception("Invalid owner id")
         raise HTTPException(status_code=400, detail="Invalid owner id")
     if users_crud.get_user(db, user_id=current_user.id) is None:
+        logging.exception("User not found")
         raise HTTPException(status_code=404, detail="User not found")
     db_conferences = crud.get_conferences_by_owner_id(db, owner_id=current_user.id)
     if db_conferences is None or len(db_conferences) == 0:
+        logging.exception("No conferences found")
         raise HTTPException(status_code=404, detail="Conference not found")
-    logging.info("Conferences retrieved by owner id: " + current_user.email)
+    logging.info("Conferences retrieved for owner id: " + users_crud.get_user_uuid_by_id(db, user_id=current_user.id))
     return db_conferences
 
 # update conference by conference id
 @router.put("/conferences", response_model=schemas.Conference)
 def update_conference(conference: schemas.ConferenceUpdate, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
     if all(value is None for value in dict(conference).values()):
+        logging.exception("Invalid request body")
         raise HTTPException(status_code=400, detail="Invalid request body")
     db_conference = crud.get_conference_by_uuid(db, uuid=conference.id, owner_id=current_user.id)
     if db_conference is None:
+        logging.exception("Conference not found")
         raise HTTPException(status_code=404, detail="Conference not found")
     if conference.start_date is not None and conference.end_date is not None:
         if conference.start_date > conference.end_date or conference.start_date < date.today():
+            logging.exception("Invalid date range")
             raise HTTPException(status_code=400, detail="Invalid date range")
     updated_conference = crud.update_user_conference(db=db, conference=conference, uuid=conference.id, owner_id=current_user.id)
     logging.info("Conference updated: " + db_conference.name)
@@ -82,11 +95,14 @@ def update_conference(conference: schemas.ConferenceUpdate, db: Session = Depend
 @router.delete("/conferences/{conference_id}")
 def delete_conference_owner_id_conference_id(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
     if current_user.id <= 0:
+        logging.exception("Invalid id")
         raise HTTPException(status_code=400, detail="Invalid id")
     if not users_crud.get_user(db, user_id=current_user.id):
+        logging.exception("User not found")
         raise HTTPException(status_code=404, detail="User not found")
     db_conference = crud.get_conference_by_uuid(db,owner_id=current_user.id, uuid=conference_id)
     if db_conference is None:
+        logging.exception("Conference not found")
         raise HTTPException(status_code=404, detail="Conference not found")
     logging.info("Conference deleted: " + db_conference.name)
     return crud.delete_conference(db=db, owner_id=current_user.id, uuid=conference_id)
@@ -96,6 +112,7 @@ def delete_conference_owner_id_conference_id(conference_id: str, db: Session = D
 def generate_qr_code(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
     conference = crud.get_conference_by_uuid(db, uuid=conference_id, owner_id=current_user.id)
     if conference is None:
+        logging.exception("Conference not found")
         raise HTTPException(status_code=404, detail="Conference not found")
     qr = qrcode.QRCode(
         version=1,
@@ -123,5 +140,4 @@ def generate_qr_code(conference_id: str, db: Session = Depends(get_db), current_
     }
 
     logging.info("QR code generated: " + conference.name)
-
     return StreamingResponse(img_byte_arr, media_type="image/png", headers=headers)
