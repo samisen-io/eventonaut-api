15424a3545870b4b8c6c35856bad1579c319c250
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2023-12-06 12:38:08 +0530

    Add pinecone-client to requirements.txt and update ai_models.py

--- app/data_ingestion.py
+++ app/data_ingestion.py
@@ -4,16 +4,24 @@ import os
 import csv
 import csv
 from dotenv import load_dotenv
+from fastapi import HTTPException
 from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Chroma, Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from app.routers.sessions import get_sessions_by_conference_id
+import pinecone
 
 load_dotenv()
 api_key = os.environ.get('OPENAI_API_KEY')
 if not api_key:
     print('OpenAI API key not found in environment variables.')
     exit()
+# initialize pinecone
+pinecone.init(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment = os.environ.get("PINECONE_API_ENV")
+)
+# initialize embedding function
 embedding_function = OpenAIEmbeddings()
 
 def find_delimiter(file_path):
@@ -30,7 +38,14 @@ def file_path_in_files(conference_id):
     files_folder = os.path.join(app_folder, 'files')
     if not os.path.exists(files_folder):
         os.makedirs(files_folder)        
-    file_path = os.path.join(files_folder, 'sessions'+str(conference_id)+'.csv')
+    file_path = os.path.join(files_folder, 'sessions-'+str(conference_id)+'.csv')
+    return file_path
+
+def file_path_in_files_csv(conference_id):
+    file_path = file_path_in_files(conference_id)
+    # Check if file exists, then delete it
+    if os.path.isfile(file_path):
+        os.remove(file_path)
     return file_path
 
 def file_path_in_files_json(conference_id):
@@ -38,7 +53,7 @@ def file_path_in_files_json(conference_id):
     files_folder = os.path.join(app_folder, 'files')
     if not os.path.exists(files_folder):
         os.makedirs(files_folder)        
-    file_path = os.path.join(files_folder, 'sessions_'+str(conference_id)+'.json')
+    file_path = os.path.join(files_folder, 'sessions-'+str(conference_id)+'.json')
     # Check if file exists, then delete it
     if os.path.isfile(file_path):
         os.remove(file_path)
@@ -59,16 +74,17 @@ def write_data_to_json(conference_id, db):
         json.dump(json.loads(json_result), f, indent=4)
     
 def write_data_to_csv(conference_id, db):
-    file_path = file_path_in_files_json(conference_id)
+    file_path = file_path_in_files_csv(conference_id)
     result = get_sessions_by_conference_id(conference_id, db)
+    if not result:
+        raise HTTPException(status_code=404, detail="No sessions found for this conference_id")
     with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['id', 'name', 'end_time', 'date', 'conference_id', 'start_time', 'description', 'location', 'owner_id']
-        fieldnames = ['name', 'description', 'location', 'date', 'start_time', 'end_time', 'tags', 'speakers']
+        fieldnames = ['uuid','name', 'description', 'location', 'date', 'start_time', 'end_time', 'tags', 'speakers']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for session in result:
             session_dict = session.__dict__
-            session_dict.pop('_sa_instance_state', None)
+            # session_dict.pop('_sa_instance_state', None)
             for field in ['end_time', 'date', 'created_on', 'updated_on', 'start_time']:
                 if field in session_dict:
                     if isinstance(session_dict[field], datetime.date):
@@ -77,6 +93,29 @@ def write_data_to_csv(conference_id, db):
                         session_dict[field] = session_dict[field].strftime("%H:%M:%S")
                     elif isinstance(session_dict[field], datetime.datetime):
                         session_dict[field] = session_dict[field].strftime("%Y-%m-%d %H:%M:%S")
-            session_dict.pop('created_on', None)
-            session_dict.pop('updated_on', None)
+            discard = ['id', 'conference_id', 'owner_id', 'created_on', 'updated_on','_sa_instance_state']
+            for field in discard:
+                session_dict.pop(field, None)
             writer.writerow(session_dict)  
+            
+def create_vector_db(conference_id):
+    # get the file path
+    file_path = file_path_in_files(conference_id)
+    # find the delimiter
+    delimiter = find_delimiter(file_path)
+    # split the csv file into chunks
+    loader = CSVLoader(file_path = file_path, encoding='utf-8',source_column = 'uuid',csv_args = {'delimiter': delimiter})
+    data = loader.load()
+    # create pinecone index
+    index_name = "sessions-"+str(conference_id)
+    try:
+        pinecone.create_index(name = index_name, dimension=1536, metric = "cosine", shards=1)
+    except Exception as e:
+        raise HTTPException(status_code=409, detail="Unable to create index: " + str(e))
+    # upload the data to pinecone
+    try:
+        Pinecone.from_documents(documents = data, index_name=index_name, embedding=embedding_function)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Unable to upload documents: " + str(e))
+    return index_name
+    
