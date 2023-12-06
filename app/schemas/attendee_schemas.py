9dd241bfdf8450fd26bf936598a5d61bb4a4d728
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-12-06 18:33:24 +0530

    Upload image for attendee (#73)
    * Add image upload functionality
    
    * Fix attendee profile image upload
    
    * Add azure-storage-blob and azure-identity
    dependencies
    
    * Refactor upload_image route to upload_file
    
    * Add HTTPException for error handling
    
    * Update HTTPException status code to 502
    
    * Update upload_file endpoint status code

--- app/schemas/attendee_schemas.py
+++ app/schemas/attendee_schemas.py
@@ -123,7 +123,6 @@ class AttendeeUpdate(BaseModel):
     bio: str | None = None
     share_my_profile: bool | None = None
     share_my_agenda: bool | None = None
-    profile_image_url: str | None = None
 
     @validator('first_name')
     def first_name_is_not_empty(cls, v):
@@ -166,12 +165,6 @@ class AttendeeUpdate(BaseModel):
                 raise HTTPException(status_code=400, detail="Invalid bio")
             elif len(v) > 256:
                 raise HTTPException(status_code=400, detail="Bio too long")
-        return v
-    
-    @validator('profile_image_url')
-    def profile_image_url_is_not_empty(cls, v):
-        if v is not None and v == "string":
-            raise HTTPException(status_code=400, detail="Invalid profile image url")
         return v
 
 #pydantic model for attendee
