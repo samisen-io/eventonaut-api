fe204639464f732f9511412e43c62f049a56895b
praveen1621 <praveen@samisen.io>
2023-12-06 16:45:22 +0530

    Fix typo in attendee profiles endpoint

--- app/routers/attendee_conference.py
+++ app/routers/attendee_conference.py
@@ -42,7 +42,7 @@ def delete_attendee_conference_by_attendee_id_and_conference_id(conference_code:
         raise HTTPException(status_code=404, detail="No conference found")
     return crud.delete_attendee_conference_by_attendee_id_and_conference_id(db=db, attendee_id=current_user.id, conference_code=conference_code)
 
-@router.get("/attendee/profiles/", response_model=list[schemas.Attendee])
+@router.get("/attendee/profiles", response_model=list[schemas.Attendee])
 def get_attendee_profiles_for_session_id(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["attendee"])):
     if not conferences_crud.get_conference_by_conference_uuid(db, uuid=conference_id):
         raise HTTPException(status_code=400, detail="Conference not found")
