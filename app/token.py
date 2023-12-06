a27dd7887cb97d67e0388fbd19b42b5e821ef9e4
praveen1621 <praveen@samisen.io>
2023-12-06 15:49:53 +0530

    Update token cache TTL

--- app/token.py
+++ app/token.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 from app.schemas.token_schemas import Token, TokenData
 from pydantic import BaseModel, ValidationError
 
-token_cache = TTLCache(maxsize=1000, ttl=timedelta(days=10))
+token_cache = TTLCache(maxsize=1000, ttl=864000)
     
 load_dotenv()
 REFRESH_TOKEN_SECRET_KEY = os.getenv("REFRESH_TOKEN_SECRET_KEY")
