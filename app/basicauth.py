cfb90c8627b016fa82358a421451cfa4de319779
praveen1621 <praveen@samisen.io>
2023-12-07 11:04:36 +0530

    Refactor basic authentication credentials loading

--- app/basicauth.py
+++ app/basicauth.py
@@ -1,24 +1,26 @@
 import secrets
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import os
+from dotenv import load_dotenv
 
 security = HTTPBasic()
 
-user_credentials = {
-    "client_id": "foo",
-    "client_secret": "bar",
-}
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 def basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
     global user_credentials
     current_username_bytes = credentials.username.encode("utf-8")
-    correct_username_bytes = user_credentials["client_id"].encode("utf-8")
+    correct_username_bytes = client_id.encode("utf-8")
     is_username_correct = secrets.compare_digest(
         current_username_bytes, correct_username_bytes
     )
 
     current_password_bytes = credentials.password.encode("utf-8")
-    correct_password_bytes = user_credentials["client_secret"].encode("utf-8")
+    correct_password_bytes = client_secret.encode("utf-8")
     is_password_correct = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
