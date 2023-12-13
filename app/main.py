805c0490c5fa5628ca70c7b3646008125f90b003
praveen1621 <praveen@samisen.io>
2023-12-13 12:15:27 +0530

    Add speakers router to main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 from app.oauth2 import get_current_active_user
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, attendee, agenda
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference
+from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, speakers
 from . import upload_image
 from .crud import logout_token_crud
 
@@ -23,6 +23,7 @@ app.add_middleware(
 app.include_router(upload_image.router)
 app.include_router(users.router)
 app.include_router(conferences.router)
+app.include_router(speakers.router)
 app.include_router(sessions.router)
 app.include_router(settings.router)
 # app.include_router(ai_models.router, dependencies=[Depends(get_current_active_user)])
