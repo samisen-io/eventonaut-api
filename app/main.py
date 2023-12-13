d3a7141b2bbf48d43594163933ab1312b9fa3ce0
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2023-12-13 17:55:28 +0530

    Feat: Created Client Table and Implemented CRUD Operations (#80)
    * Add client router to main.py
    
    * Add Client model and relationships
    
    * Add client CRUD operations
    
    * Add client API routes
    
    * Add client schemas for validation and data models
    
    * Add relationship between Client and Conference models
    
    * Remove unnecessary code in delete_client function
    
    * Add client_id to create and update conference
    
    * Fix validation for contact phone length
    
    * Add client_id field to ConferenceCreate and ConferenceUpdate schemas
    
    * Fix client email validation and registration check

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 from app.oauth2 import get_current_active_user
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, attendee, agenda
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference
+from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client
 from . import upload_image
 from .crud import logout_token_crud
 
@@ -22,6 +22,7 @@ app.add_middleware(
 # Add the routers to the application with authentication middleware
 app.include_router(upload_image.router)
 app.include_router(users.router)
+app.include_router(client.router)
 app.include_router(conferences.router)
 app.include_router(sessions.router)
 app.include_router(settings.router)
