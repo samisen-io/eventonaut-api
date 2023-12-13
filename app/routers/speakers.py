69e1dd970db1da74b388a1e0e8b9c83ec6c3a8b3
praveen1621 <praveen@samisen.io>
2023-12-13 12:15:42 +0530

    Add speaker API endpoints

---
+++ app/routers/speakers.py
@@ -0,0 +1,45 @@
+from ..dependencies import get_db
+from .. import models
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status
+from ..schemas import speaker_schemas as schemas
+from ..crud import speakers_crud as crud, conferences_crud
+
+router = APIRouter(tags=["speakers"])
+
+@router.post("/speakers", response_model=schemas.Speaker)
+def create_speaker(speaker: schemas.SpeakerCreate, db: Session = Depends(get_db)):
+    return crud.create_speaker(db=db, speaker=speaker)
+
+@router.get("/speakers", response_model=list[schemas.Speaker])
+def get_speakers(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    speakers = crud.get_all_speakers(db, offset=offset, limit=limit)
+    if speakers is None or len(speakers) == 0:
+        raise HTTPException(status_code=404, detail="Speaker not found")
+    return speakers
+
+@router.get("/speakers/{speaker_id}", response_model=schemas.Speaker)
+def get_speaker(speaker_id: str, db: Session = Depends(get_db)):
+    db_speaker = crud.get_speaker(db, speaker_id=speaker_id)
+    if db_speaker is None:
+        raise HTTPException(status_code=404, detail="Speaker not found")
+    return db_speaker
+
+@router.put("/speakers", response_model=schemas.Speaker)
+def update_speaker(speaker: schemas.SpeakerUpdate, db: Session = Depends(get_db)):
+    if all(value is None for value in dict(speaker).values()):
+        raise HTTPException(status_code=400, detail="Invalid request body")
+    db_speaker = crud.get_speaker(db, speaker_id=speaker.id)
+    if db_speaker is None:
+        raise HTTPException(status_code=404, detail="Speaker not found")
+    if speaker.conference_id is not None:
+        if conferences_crud.get_conf_by_uuid(db, speaker.conference_id) is None:
+            raise HTTPException(status_code=404, detail="Conference not found")
+    return crud.update_speaker(db=db, speaker=speaker)
+
+@router.delete("/speakers/{speaker_id}")
+def delete_speaker(speaker_id: str, db: Session = Depends(get_db)):
+    db_speaker = crud.get_speaker(db, speaker_id=speaker_id)
+    if db_speaker is None:
+        raise HTTPException(status_code=404, detail="Speaker not found")
+    return crud.delete_speaker(db=db, speaker_id=speaker_id)
