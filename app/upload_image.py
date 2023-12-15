b7af528133e155c4e9a1d7809390eeaa5c5a47cc
praveen1621 <praveen@samisen.io>
2023-12-15 15:44:13 +0530

    Add dotenv and update file upload validation

--- app/upload_image.py
+++ app/upload_image.py
@@ -5,13 +5,17 @@ from .dependencies import get_db
 from app.oauth2 import get_current_active_user
 from .schemas.user_schemas import UserAuthentication as User
 from .crud import attendee_crud as crud
+from dotenv import load_dotenv
+import os
 
 router = APIRouter(tags=["file"])
+
+load_dotenv()
 
 @router.post("/upload_file", status_code=201)
 def upload_file(file: UploadFile = File(...), current_user: User = Security(get_current_active_user, scopes=["attendee"])):
     try:
-        connect_str = "DefaultEndpointsProtocol=https;AccountName=conferencebuddydev;AccountKey=AkI79mMDMpg+Xi75ez89PO5HuhqOLdB5cOtNhsARwapbPVcQ4AuzVkpJ7jaB+iIZm8WPV7HE4CSB+AStiZkq/A==;EndpointSuffix=core.windows.net"
+        connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
         container_name = "attende-profile-images"
         container_client = blob_service_client.get_container_client(container_name)
@@ -23,6 +27,10 @@ def upload_file(file: UploadFile = File(...), current_user: User = Security(get_
             pass  # The container already exists
 
         file_extension = file.filename.split(".")[-1]  # Get the file extension
+
+        if file_extension not in ["jpg", "jpeg", "png"]:
+            raise HTTPException(status_code=400, detail="Invalid file type. Only jpg, jpeg, and png are allowed.")
+
         blob_name = f"profile-{current_user.uuid}.{file_extension}"  # Append the file extension to the blob name
         blob_client = blob_service_client.get_blob_client(container_name, blob_name)
 
