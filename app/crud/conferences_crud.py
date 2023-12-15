062b9bcc654e146f9678a292e6b79e911b003415
praveen1621 <praveen@samisen.io>
2023-12-15 12:21:16 +0530

    Remove 'client_id' from returned conference objects

--- app/crud/conferences_crud.py
+++ app/crud/conferences_crud.py
@@ -67,14 +67,20 @@ def create_user_conference(db: Session, conference: schemas.ConferenceCreate, us
     return db_conference
 
 def get_conf_by_uuid(db:Session, conference_id: str):
-    return db.query(models.Conference).filter(models.Conference.uuid == conference_id).first()
+    conference = db.query(models.Conference).filter(models.Conference.uuid == conference_id).first()
+    conference.__dict__.pop('client_id')
+    return conference
 
 # get conference by uuid and owner id
 def get_conference_by_uuid(db: Session, uuid: str, owner_id: int):
-    return db.query(models.Conference).filter(models.Conference.uuid == uuid, models.Conference.owner_id == owner_id).first()
+    conference = db.query(models.Conference).filter(models.Conference.uuid == uuid, models.Conference.owner_id == owner_id).first()
+    conference.__dict__.pop('client_id')
+    return conference
 
 def get_conference_by_conference_uuid(db: Session, uuid: str):
-    return db.query(models.Conference).filter(models.Conference.uuid == uuid).first()
+    conference = db.query(models.Conference).filter(models.Conference.uuid == uuid).first()
+    conference.__dict__.pop('client_id')
+    return conference
 
 # delete conference by conference id
 def delete_conference(db: Session, owner_id: int, uuid: str):
