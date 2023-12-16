8d3d44012fb84f235f82690d94eeed8e58912470
praveen1621 <praveen@samisen.io>
2023-12-16 11:46:08 +0530

    Add result schemas and update result_crud.py

--- app/crud/result_crud.py
+++ app/crud/result_crud.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from .. import models
+from ..schemas import result_schemas
 
 def get_objects(db: Session, objects: list[str]):
-    tables = {
+    models_table = {
         'usr': models.User,
         'evt': models.Conference,
         'ses': models.Session,
@@ -17,20 +18,34 @@ def get_objects(db: Session, objects: list[str]):
         'spk': models.Speakers
     }
 
+    schemas_table = {
+        'usr': result_schemas.User,
+        'evt': result_schemas.Event,
+        'ses': result_schemas.Session,
+        'set': result_schemas.Settings,
+        'cnf': result_schemas.Conference_Files,
+        'atd': result_schemas.Attendee,
+        'ate': result_schemas.Attendee_Conferences,
+        'aga': result_schemas.Agenda,
+        'ags': result_schemas.AgendaSession,
+        'ltk': result_schemas.LogoutToken,
+        'cli': result_schemas.Client,
+        'spk': result_schemas.Speakers
+    }
+
     final_objects = []
     rank = 1
-
     for obj in objects:
         code = obj[:3]
-        if code not in tables:
+        if code not in models_table.keys():
             return False
         else:
-            db_obj = db.query(tables[code]).filter(tables[code].uuid == obj).first()
+            db_obj = db.query(models_table[code]).filter(models_table[code].uuid == obj).first()
             if db_obj is None:
                 return False
             db_obj = db_obj.__dict__
             db_obj['rank'] = rank
             rank += 1
-            final_objects.append(db_obj)
+            final_objects.append(schemas_table[code](**db_obj))
 
     return final_objects
