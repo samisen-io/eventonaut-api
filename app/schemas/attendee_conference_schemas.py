4aec05dab728cdf08adb4beb1e8124b8f933fc63
praveen1621 <praveen@samisen.io>
2024-01-11 17:53:56 +0530

    Refactor attendee conference schemas and add conference model

--- app/schemas/attendee_conference_schemas.py
+++ app/schemas/attendee_conference_schemas.py
@@ -1,27 +1,32 @@
-from pydantic import BaseModel, validator, Field
+from pydantic import BaseModel, field_validator, Field
 from fastapi import HTTPException, status
+from datetime import date
 
 class AttendeeConferenceCreate(BaseModel):
-    conference_code: str
+    conference_identifier: str
     
-    @validator('conference_code')
-    def validate_conference_code(cls, v):
-        if v is None or v.strip() == '' or v == 'string':
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Conference code cannot be null')
+    @field_validator('conference_identifier')
+    def validate_conference_identifier(cls, v):
+        if v.strip() == '':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Conference identifier cannot be empty")
+        elif v == 'string':
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Conference identifier cannot be string")
+        elif len(v) < 6:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid conference identifier")
         return v
 
-class AttendeeConferenceCreateId(BaseModel):
-    conference_id: str
-    
-    @validator('conference_id')
-    def validate_conference_id(cls, v):
-        if v is None or v.strip() == '' or v == 'string':
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Conference id cannot be null')
-        return v
+class Conference(BaseModel):
+    id: str = Field(alias='uuid', serialization_alias='id')
+    name: str
+    location: str
+    start_date: date
+    end_date: date
+    description: str
+    conference_logo: str
 
 class AttendeeConference(BaseModel):
     uuid: str = Field(serialization_alias='id')
     attendee_id: str
-    conference: dict
+    conference: Conference
     class Config:
         orm_mode = True
