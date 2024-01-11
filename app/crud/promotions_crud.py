64c9dd7250d3ef3ed9a2a00bbfb76e9be49111a4
praveen1621 <praveen@samisen.io>
2024-01-11 13:25:03 +0530

    Add conference information to promotions

--- app/crud/promotions_crud.py
+++ app/crud/promotions_crud.py
@@ -5,17 +5,24 @@ import uuid
 from datetime import datetime
 
 def get_promotion(db: Session, promotion_id: str):
-    return db.query(models.Promotions).filter(models.Promotions.uuid == promotion_id).first()
+    promotion = db.query(models.Promotions).filter(models.Promotions.uuid == promotion_id).first()
+    if promotion is not None:
+        promotion = add_conference_to_promotion(db, promotion)
+    return promotion
 
 def get_promotion_by_conference(db: Session, conference_id: str):
     conference = db.query(models.Conference).filter(models.Conference.uuid == conference_id).first()
     return db.query(models.Promotions).filter(models.Promotions.conference_id == conference.id).first()
 
 def get_promotions(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Promotions).order_by(models.Promotions.rank).offset(skip).limit(limit).all()
+    promotions = db.query(models.Promotions).order_by(models.Promotions.rank).offset(skip).limit(limit).all()
+    promotions = add_conference_to_promotion(db, promotions)
+    return promotions
 
 def get_all_promotions(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Promotions).offset(skip).limit(limit).all()
+    promotions = db.query(models.Promotions).order_by(models.Promotions.rank).offset(skip).limit(limit).all()
+    promotions = add_conference_to_promotion(db, promotions)
+    return promotions
 
 def create_promotion(db: Session, promotion: promotion_schemas.PromotionCreate):
     db_promotion = models.Promotions(**promotion.model_dump())
@@ -25,12 +32,15 @@ def create_promotion(db: Session, promotion: promotion_schemas.PromotionCreate):
     db.add(db_promotion)
     db.commit()
     db.refresh(db_promotion)
-    return db_promotion
+    promotion = add_conference_to_promotion(db, db_promotion)
+    return promotion
 
 def update_promotion(db: Session, promotion: promotion_schemas.PromotionUpdate):
     db_promotion = db.query(models.Promotions).filter(models.Promotions.uuid == promotion.id).first()
+    db.refresh(db_promotion)
     promotion: dict = promotion.model_dump()
     promotion_rank = promotion.pop('rank')
+    conference_id = promotion.pop('conference_id')
     promotion.pop('id')
     updates = promotion
     for key, value in updates.items():
@@ -40,14 +50,26 @@ def update_promotion(db: Session, promotion: promotion_schemas.PromotionUpdate):
     if promotion_rank != 0:
         db_promotion.rank = promotion_rank
 
+    if conference_id is not None:
+        db_promotion.conference_id = db.query(models.Conference).filter(models.Conference.uuid == conference_id).first().id
+
     db_promotion.updated_on = datetime.utcnow()
 
     db.commit()
     db.refresh(db_promotion)
-    return db_promotion
+    promotion = add_conference_to_promotion(db, db_promotion)
+    return promotion
 
 def delete_promotion(db: Session, promotion_id: str):
     db_promotion = db.query(models.Promotions).filter(models.Promotions.uuid == promotion_id).first()
     db.delete(db_promotion)
     db.commit()
     return True
+
+def add_conference_to_promotion(db: Session, promotion):
+    if isinstance(promotion,list):
+        for p in promotion:
+            p.conference_id = db.query(models.Conference).filter(models.Conference.id == p.conference_id).first().uuid
+    else:
+        promotion.conference_id = db.query(models.Conference).filter(models.Conference.id == promotion.conference_id).first().uuid
+    return promotion
