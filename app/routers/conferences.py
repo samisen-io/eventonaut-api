93ee465d06de3f1b6290cc94ed3ac55cf25f8ac8
praveen1621 <praveen@samisen.io>
2024-01-23 15:44:15 +0530

    Remove unused code in conferences.py

--- app/routers/conferences.py
+++ app/routers/conferences.py
@@ -148,7 +148,6 @@ def generate_qr_code(conference_id: str, db: Session = Depends(get_db), current_
     logging.info("QR code generated: " + conference.name)
     return StreamingResponse(img_byte_arr, media_type="image/png", headers=headers)
 
-# get conference by conference id
 @router.get("/conferences/{conference_id}", response_model=schemas.Conference)
 def get_conference_by_conference_id(conference_id: str, db: Session = Depends(get_db), basic_auth = Depends(basicauth.basic_auth)):    
     conference = crud.get_conference_by_conference_uuid(db, uuid=conference_id)
