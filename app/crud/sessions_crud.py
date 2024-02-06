437aa476a14a15566584ba0df5c63d1e47a1f054
praveen1621 <praveen@samisen.io>
2024-02-06 16:24:03 +0530

    changed get sessions by conference to basicauth

--- app/crud/sessions_crud.py
+++ app/crud/sessions_crud.py
@@ -78,8 +78,8 @@ def get_session_by_uuid_id(db: Session, uuid: int, owner_id: int):
     db_session = db.query(models.Session).filter(models.Session.uuid == uuid, models.Session.owner_id == owner_id, models.Session.isarchived == False).first()
     return db_session
 
-def get_all_sessions_by_uuid_id(db: Session, conference_uuid: str, owner_id: int):
-    conference = db.query(models.Conference).filter(models.Conference.uuid == conference_uuid, models.Conference.owner_id == owner_id, models.Conference.isarchived == False).first()
+def get_all_sessions_by_uuid_id(db: Session, conference_uuid: str):
+    conference = db.query(models.Conference).filter(models.Conference.uuid == conference_uuid, models.Conference.isarchived == False).first()
     if conference is None:
         return None
     db_sessions = db.query(models.Session).filter(models.Session.conference_id == conference.id, models.Session.isarchived == False).all()
