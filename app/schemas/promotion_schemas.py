d1125a864b7e93e9fd82a991ce59358bd9f0b337
praveen1621 <praveen@samisen.io>
2024-02-06 11:07:13 +0530

    Add field validation for empty values in Promotion schemas

--- app/schemas/promotion_schemas.py
+++ app/schemas/promotion_schemas.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, ValidationInfo
 from datetime import date
-import uuid
+from fastapi import HTTPException, status
+import logging
 
 class PromotionBase(BaseModel):
     todate: date
@@ -8,6 +9,13 @@ class PromotionBase(BaseModel):
     image_url: str
     promotion_name: str
     rank: int = Field(gt=0, lt=6)
+    
+    @field_validator('promotion_name','image_url')
+    def check_empty(cls, v, info: ValidationInfo):
+        if v.strip() == "":
+            logging.exception(f"{info.field_name} cannot be empty")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"{info.field_name} cannot be empty")
+        return v
 
 class PromotionCreate(PromotionBase):
     conference_id: str
@@ -20,6 +28,12 @@ class PromotionUpdate(PromotionBase):
     image_url: str | None = None
     promotion_name: str | None = None
     rank: int = Field(gt=0, lt=6, default=0)
+    
+    @field_validator('promotion_name','image_url')
+    def check_empty(cls, v, info: ValidationInfo):
+        if v is not None and v.strip() == "":
+            return None
+        return v
 
 class Promotion(PromotionBase):
     uuid: str = Field(serialization_alias='id')
