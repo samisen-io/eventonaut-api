49a64bbc801a586260a6bc6d9e824a94a367e6fe
Abhishek Katta <abhishekk@samisen.io>
2024-02-18 23:11:20 +0530

    migrations done

--- app/migrations/organization_migration.py
+++ app/migrations/organization_migration.py
@@ -3,80 +3,46 @@ from sqlalchemy.orm import sessionmaker
 from app.models import User, Organization, Organization_User
 import uuid
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+
 DATABASE_URL = os.getenv("DATABASE_URL_ADDRESS")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def main():
-    # Create Session
     session = SessionLocal()
     try:
-        # Iterate over all users
         for user in session.query(User).all():
-            print(user)
-            # Check if an organization with the same name already exists
-            organization = session.query(Organization).filter_by(name=user.company).first()
-            print(organization)
-            if organization is None:
-                # If not, create a new organization
-                organization = Organization(
-                    name=user.company,
-                    business_type=user.business_type,
-                    uuid='org-' + str(uuid.uuid4()),
-                    created_on=datetime.now(),
-                    updated_on=datetime.now()
-                )
-                # Add the new organization to the session
-                session.add(organization)
-                # Flush the session to assign an ID to the new organization
-                session.flush()
-            # Create a new organization_user record for the new organization
-            organization_user = Organization_User(
-                organization_id=organization.id,
-                user_id=user.id,
-                uuid='ous-' + str(uuid.uuid4()),
-                created_on=datetime.now(),
-                updated_on=datetime.now()
-            )
-            # Add the new organization_user record to the session
-            session.add(organization_user)
-        # Commit the changes
-        session.commit()
-        for user in session.query(User).all():
-            # Check if an organization with the same name already exists
-            organization = session.query(Organization).filter_by(name=user.company).first()
-            if organization is None:
-                # If not, create a new organization
-                organization = Organization(
-                    name=user.company,
-                    business_type=user.business_type,
-                    uuid='org-' + str(uuid.uuid4()),
-                    created_on=datetime.now(),
-                    updated_on=datetime.now()
-                )
-                # Add the new organization to the session
-                session.add(organization)
-                # Flush the session to assign an ID to the new organization
-                session.flush()
-            # Create a new organization_user record for the new organization
-            organization_user = Organization_User(
-                organization_id=organization.id,
-                user_id=user.id,
-                uuid='ous-' + str(uuid.uuid4()),
-                created_on=datetime.now(),
-                updated_on=datetime.now()
-            )
-            # Add the new organization_user record to the session
-            session.add(organization_user)
-        # Commit the changes
+            print(f"Processing User ID: {user.id}")
+            if user.company and user.role != 'attendee':
+                organization = session.query(Organization).filter_by(name=user.company).first()
+                if organization is None:
+                    organization = Organization(
+                        name=user.company,
+                        uuid='org-' + str(uuid.uuid4()),
+                        created_on=datetime.now(),
+                        updated_on=datetime.now(),
+                        business_type=user.business_type if user.business_type else None
+                    )
+                    session.add(organization)
+                    session.flush()
+
+                user_exists = session.query(User.id).filter_by(id=user.id).scalar() is not None
+                if user_exists:
+                    organization_user = Organization_User(
+                        organization_id=organization.id,
+                        user_id=user.id,
+                        uuid='ous-' + str(uuid.uuid4()),
+                        created_on=datetime.now(),
+                        updated_on=datetime.now()
+                    )
+                    session.add(organization_user)
+        print(f"Processed {session.query(User).count()} users.")
         session.commit()
     except exc.IntegrityError as e:
         session.rollback()
-        print(str(e))
+        print(f"IntegrityError: {str(e)}")
     finally:
         session.close()
 
--- app/migrations/role_migration.py
+++ app/migrations/role_migration.py
@@ -11,33 +11,24 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def main():
-    # Create Session
     session = SessionLocal()
     try:
-        # Insert roles
         roles = [
             {"name": "ATTENDEE", "description": "Eventonaut application user"},
             {"name": "ORGANIZATION_ADMIN", "description": "Organization creator and Supreme leader of a particular Organizer"},
             {"name": "ORGANIZATION_USER", "description": "This user can have limited access on Command Center"},
         ]
         for role_data in roles:
-            # Generate UUID with 'rol-' prefix
-            role_data["uuid"] = "rol-" + str(uuid.uuid4())
-            # Add created_on and updated_on fields
-            role_data["created_on"] = datetime.now()
-            role_data["updated_on"] = datetime.now()
-
-            # Check if role with the same uuid already exists
-            role = session.query(Role).filter_by(uuid=role_data["uuid"]).first()
+            role = session.query(Role).filter_by(name=role_data["name"]).first()
             if role is None:
-                # If not, create a new role
+                role_data["uuid"] = "rol-" + str(uuid.uuid4())
+                role_data["created_on"] = datetime.now()
+                role_data["updated_on"] = datetime.now()
                 role = Role(**role_data)
                 session.add(role)
-        session.flush()
+        session.commit()
 
-        # Iterate over all users
         for user in session.query(User).all():
-            # Determine role based on user's role attribute
             if user.role == 'organizer':
                 role_name = 'ORGANIZATION_ADMIN'
             elif user.role == 'attendee':
@@ -45,22 +36,18 @@ def main():
             else:
                 continue
 
-            # Get role object
             role = session.query(Role).filter_by(name=role_name).first()
             if role is None:
                 continue
 
-            # Check if user already has this role
             user_role = session.query(User_Role).filter_by(user_id=user.id, role_id=role.id).first()
             if user_role is not None:
                 continue
 
-            # Check if user already has two roles
             user_roles = session.query(User_Role).filter_by(user_id=user.id).all()
             if len(user_roles) >= 2:
                 continue
 
-            # Create a new user_role record
             user_role = User_Role(
                 role_id=role.id,
                 user_id=user.id,
@@ -68,10 +55,8 @@ def main():
                 created_on=datetime.now(),
                 updated_on=datetime.now()
             )
-            # Add the new user_role record to the session
             session.add(user_role)
             print(f"Added user_role record for user {user.id} with role {role.name}")
-        # Commit the changes
         session.commit()
         print("Changes committed successfully")
     except exc.IntegrityError as e:
@@ -82,5 +67,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # Add prints in console
     print("Print statements added")
