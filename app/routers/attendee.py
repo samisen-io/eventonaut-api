9cfca3ddb936fa38b873d174396b4901b05c52bf
praveen1621 <praveen@samisen.io>
2024-02-21 10:21:39 +0530

    Update attendee endpoint to return response model

--- app/routers/attendee.py
+++ app/routers/attendee.py
@@ -50,7 +50,7 @@ def get_attendee_by_id(db: Session = Depends(get_db), current_user: User = Secur
     return db_attendee
 
 # update attendee by email
-@router.put("/attendee")
+@router.put("/attendee", response_model=schemas.Attendee)
 def update_attendee_by_id(attendee: schemas.AttendeeUpdate, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["attendee"])):
     if all(value is None for value in dict(attendee).values()):
         logging.exception("Invalid request body")
@@ -88,16 +88,3 @@ def delete_attendee_by_id(db: Session = Depends(get_db), current_user: User = Se
     deleted_attendee = crud.delete_attendee_by_uuid(db, attendee_id=current_user.id)
     logging.info("Attendee deleted: " + db_attendee.uuid)
     return deleted_attendee
-
-@router.get("/attendee/fill the db")
-def fill_the_db(db: Session = Depends(get_db)):
-    db_users = db.query(models.User).filter(models.User.role == 'attendee').all()
-    db_attendees = db.query(models.Attendee).all()
-    
-    for user in db_users:
-        for attendee in db_attendees:
-            if user.id == attendee.user_id:
-                user.profile_image_url = attendee.profile_image_url
-                break
-    db.commit()
-    return {"message": "DB filled"}
