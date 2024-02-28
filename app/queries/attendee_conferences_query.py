709a5702f37759b59aa8fe92b84a9d0c1ec37a59
Abhishek Katta <abhishekk@samisen.io>
2024-02-28 20:08:07 +0530

    Refactor attendee_conferences_query.py to improve readability and maintainability

--- app/queries/attendee_conferences_query.py
+++ app/queries/attendee_conferences_query.py
@@ -1,1 +1,25 @@
-query = """select u.email,u.first_name ,u.last_name,a.title , u.company , a.bio , a.share_my_profile ,a.share_my_agenda ,a.profile_image_url ,a.uuid ,u.is_active,(select upper(status) from organizer_status os where id = u.user_status_id) status from attendees a join attendee_conferences ac on ac.attendee_id  = a.id join users u on u.id = a.user_id where ac.conference_id  = (select id from conferences c where "uuid" = :conference_uuid) and a.share_my_profile = true and u.is_archived = false"""
+query = """SELECT 
+    u.email,
+    u.first_name,
+    u.last_name,
+    a.title,
+    u.company,
+    a.bio,
+    a.share_my_profile,
+    a.share_my_agenda,
+    a.profile_image_url,
+    a.uuid,
+    u.is_active,
+    (SELECT UPPER(status) FROM organizer_status os WHERE id = u.user_status_id) status
+FROM 
+    attendees a 
+JOIN 
+    attendee_conferences ac ON ac.attendee_id = a.id 
+JOIN 
+    users u ON u.id = a.user_id 
+JOIN 
+    conferences c ON c.id = ac.conference_id
+WHERE 
+    c.uuid = :conference_uuid 
+    AND a.share_my_profile = true 
+    AND u.is_archived = false"""
