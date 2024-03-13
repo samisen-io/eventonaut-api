d52c517ac8a5cd197f2dc60538c56094e3afb240
Abhishek Katta <abhishekk@samisen.io>
2024-03-13 14:30:37 +0530

    Add additional fields to the EventDocuments model and update the insert_event_document function

--- app/routers/event_documents.py
+++ app/routers/event_documents.py
@@ -7,19 +7,40 @@ from app.oauth2 import get_current_active_user
 from ..crud import conferences_crud
 from app.schemas.user_schemas import UserAuthentication as User
 from ..crud import event_documents_crud as crud
-from ..schemas.event_document_schemas import EventDocumentResponse
+from ..schemas.event_document_schemas import EventDocumentRequest, EventDocumentResponse
 from ..static_enums.blob_container_enums import BlobContainer
 
 router = APIRouter(tags=["event_documents"], prefix="/event_documents")
 
 @router.post("/", response_model=EventDocumentResponse, status_code=status.HTTP_201_CREATED)
 def create_event_document(conference_id: str, file: UploadFile = File(...), db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
-    conference = conferences_crud.get_conference_by_uuid(db, conference_id, current_user.id)
-    file.filename = f"evt-doc-{file.filename}"
-    uploaded_file = upload_file(file, db)
-    blob_url = uploaded_file["url"]
-    
-    return crud.insert_event_document(db, conference.id, blob_url)
+    try:
+        conference = conferences_crud.get_conference_by_uuid(db, conference_id, current_user.id)
+        file.filename = f"evt-doc-{file.filename}"
+        uploaded_file = upload_file(file, db)
+        blob_url = uploaded_file["url"]
+        
+        content_type = file.headers["content-type"]
+        name = file.filename
+        size = file.size / (1024 * 1024)
+        request = EventDocumentRequest(conference_id=conference.id, 
+                                        file_url=blob_url, 
+                                        file_name=name, 
+                                        content_type=content_type, 
+                                        size=size)
+        
+        response = crud.insert_event_document(db= db, request= request)
+        
+        return EventDocumentResponse(uuid=response.uuid, 
+                                     conference_uuid=conference_id,
+                                     document_url=response.document_url, 
+                                     name=response.name, 
+                                     content_type=response.content_type, 
+                                     size=f"{str(response.size)} MB")
+        
+    except Exception as e:
+        logging.error(f"An error occurred while creating event document: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail= f"{str(e)}")
 
 @router.get("/{conference_id}", response_model=list[EventDocumentResponse])
 def get_all_event_documents(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=["organizer"])):
