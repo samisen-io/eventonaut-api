a98d5da92ad648918d455c868160b0783226c9ce
praveen1621 <praveen@samisen.io>
2024-04-15 13:54:44 +0530

    Update main.py to include plan router

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from starlette.responses import JSONResponse
 from app.routers import backdrop_gallery, organization, role
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, attendee, agenda
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents
+from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents, plan
 from .crud import logout_token_crud
 
 
@@ -66,6 +66,7 @@ app.include_router(promotions.router)
 app.include_router(sponsor.router)
 app.include_router(sessions.router)
 app.include_router(settings.router)
+app.include_router(plan.router)
 # app.include_router(ai_models.router, dependencies=[Depends(get_current_active_user)])
 app.include_router(ai_models.router)
 app.include_router(attendee.router)
