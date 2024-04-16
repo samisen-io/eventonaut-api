62f5ad6a208367747024aed1eae7788cf256bb75
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2024-04-16 17:52:05 +0530

    Update file names to include user UUID (#239)
    * Update event_documents.py to include user UUID in the filename
    
    * Update session_documents.py to include user UUID in the filename
    
    * Fix URL decoding issue in upload_image.py

--- app/routers/upload_image.py
+++ app/routers/upload_image.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 from azure.storage.blob import BlobServiceClient,ContentSettings
 from fastapi import APIRouter, File, UploadFile, Depends, HTTPException, status
 from dotenv import load_dotenv
@@ -87,8 +87,8 @@ def get_blob_by_url(blob_url):
         connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
         url = urlparse(blob_url)
-        container_name = url.path.split("/")[1]
-        blob_name = url.path.split("/")[2]
+        container_name = unquote(url.path.split("/")[1])
+        blob_name = unquote(url.path.split("/")[2])
         
         blob_client = blob_service_client.get_blob_client(container_name, blob_name)
         
@@ -137,17 +137,16 @@ def move_file_from_temporary_to_permanent_container(source_container_name, dest_
  
 def get_container_name_from_url(blob_url):
     url = urlparse(blob_url)
-    return url.path.split("/")[1]
+    return unquote(url.path.split("/")[1])
     
 def delete_blob_by_url(blob_url):
     try:
         connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
         url = urlparse(blob_url)
-        container_name = url.path.split("/")[1]
-        blob_name = url.path.split("/")[2]
-        
-        blob_client = blob_service_client.get_blob_client(container_name, blob_name)
+        container_name = unquote(url.path.split("/")[1])
+        blob_name = unquote(url.path.split("/")[2])
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
         
         if not blob_client.exists():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Document not found")
