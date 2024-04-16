bcab31e2e3a10a8b42b9ffedb16fa2e04e2ff2bf
praveen1621 <praveen@samisen.io>
2024-04-16 12:30:34 +0530

    Update Organization and Conference models in models.py

--- app/models.py
+++ app/models.py
@@ -12,7 +12,6 @@ class Organization(Base):
     updated_on = Column(DateTime)
     uuid = Column(String, index=True, unique=True)
     name = Column(String, index=True)
-    company = Column(String, index=True)
     business_type = Column(String, index=True)
     description = Column(String, index=True)
     address = Column(String, index=True)
@@ -20,6 +19,7 @@ class Organization(Base):
     contact_phone = Column(String, index=True)
     logo_image_url = Column(String, index=True)
     website_url = Column(String, index=True)
+    external_id = Column(String, index=True)
 
 class User(Base):
     __tablename__ = "users"
@@ -116,6 +116,7 @@ class Conference(Base):
     information_guide = Column(String, index=True)
     conference_status_id = Column(Integer, ForeignKey("event_status.id"))
     is_archived = Column(Boolean, default=False)
+    external_id = Column(String, index=True)
 
     client = relationship("Client", back_populates="conferences")
     owner = relationship("User", back_populates="conferences")
