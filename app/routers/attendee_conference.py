c22a3a6ca1bdae5c27a9a2d89d8a974d068b3c71
ABHISHEK KATTA <117162315+abhishek-katta-ub@users.noreply.github.com>
2024-04-23 16:30:44 +0530

    Update role-based authentication scopes in routers (#260)
    * Update role-based authentication scopes in routers
    
    * Update authentication scopes in routers to include new user roles

--- app/routers/attendee_conference.py
+++ app/routers/attendee_conference.py
@@ -66,7 +66,7 @@ def delete_attendee_conference_by_attendee_id_and_conference_id(conference_ident
     return deleted_attendee_conference
 
 @router.get("/attendee/profiles", response_model=list[schemas.Attendee])
-def get_attendee_profiles_for_conference_id(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ATTENDEE.name, "organizer"])):
+def get_attendee_profiles_for_conference_id(conference_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ATTENDEE.name, "organizer"])):
     conference = conferences_crud.get_conference_by_uuid(db, uuid=conference_id,owner_id=current_user.id) if current_user.role == "organizer" else conferences_crud.get_conference_by_conference_uuid(db, uuid=conference_id)
     if conference is None:
         logging.exception("Conference not found")
--- app/routers/backdrop_gallery.py
+++ app/routers/backdrop_gallery.py
@@ -6,6 +6,7 @@ from app.crud import backdrop_gallery_crud as crud
 from app.oauth2 import get_current_active_user
 from app.schemas import backdrop_gallery_schemas as schemas
 from app.dependencies import get_db
+from app.static_enums.role import RoleEnum
 
 router = APIRouter(tags=["backdrop"])
 
@@ -16,7 +17,7 @@ def get_backdrop_by_id(backdrop_id: str, db: Session, User):
     return db_backdrop
 
 @router.get("/backdrop/{backdrop_id}", response_model=schemas.BackdropGalleryResponse)
-def Get_backdrop(backdrop_id: str, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=["organizer", "attendee"])):
+def Get_backdrop(backdrop_id: str, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ATTENDEE.name, "organizer", "attendee"])):
     db_backdrop = get_backdrop_by_id(backdrop_id, db, User)
     return schemas.BackdropGalleryResponse(conference_id=db_backdrop.conference.uuid, 
                                            backdrop_url=db_backdrop.backdrop_url, 
@@ -25,7 +26,7 @@ def Get_backdrop(backdrop_id: str, db: Session = Depends(get_db), User = Securit
                                            uuid=db_backdrop.uuid)
 
 @router.get("/backdrops/{conference_id}", response_model=List[schemas.BackdropGalleryResponse])
-def get_backdrops_by_conferene_id(conference_id: str, skip: int = 0, limit: int = 100, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=["organizer", "attendee"])):
+def get_backdrops_by_conferene_id(conference_id: str, skip: int = 0, limit: int = 100, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ATTENDEE.name,"organizer", "attendee"])):
     try:
         backdrops = crud.get_backdrops_by_conference_id(db, conference_id=conference_id, owner_id=User.id, skip=skip, limit=limit)
         response = [schemas.BackdropGalleryResponse(conference_id=conference_id, 
@@ -42,7 +43,7 @@ def get_backdrops_by_conferene_id(conference_id: str, skip: int = 0, limit: int 
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.post("/backdrop", response_model=schemas.BackdropGalleryResponse)
-def create_backdrop(backdrop: schemas.BackdropGalleryCreate, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=["organizer"])):
+def create_backdrop(backdrop: schemas.BackdropGalleryCreate, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, "organizer"])):
     try:
         modelresponse = crud.create_backdrop(db=db, backdrop=backdrop, owner_id=User.id)
         response = schemas.BackdropGalleryResponse(conference_id=backdrop.conference_id, 
@@ -59,7 +60,7 @@ def create_backdrop(backdrop: schemas.BackdropGalleryCreate, db: Session = Depen
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.put("/backdrop/{backdrop_id}", response_model=schemas.BackdropGalleryUpdateResponse)
-def update_backdrop(backdrop: schemas.BackdropGalleryUpdate, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=["organizer"])):
+def update_backdrop(backdrop: schemas.BackdropGalleryUpdate, db: Session = Depends(get_db), User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, "organizer"])):
     db_backdrop = get_backdrop_by_id(backdrop.id, db, User)
     updated_backdrop = crud.update_backdrop(db=db, backdrop=backdrop, db_backdrop=db_backdrop)
     response = schemas.BackdropGalleryUpdateResponse(backdrop_url=updated_backdrop.backdrop_url, 
@@ -69,7 +70,7 @@ def update_backdrop(backdrop: schemas.BackdropGalleryUpdate, db: Session = Depen
     return response
 
 @router.delete("/backdrop/{backdrop_id}")
-def delete_backdrop(backdrop_id: str, db: Session = Depends(get_db),  User = Security(get_current_active_user, scopes=["organizer"])):
+def delete_backdrop(backdrop_id: str, db: Session = Depends(get_db),  User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_USER.name, RoleEnum.ORGANIZATION_ADMIN.name, "organizer"])):
     db_backdrop = get_backdrop_by_id(backdrop_id, db, User)
     crud.delete_backdrop(db=db, db_backdrop=db_backdrop)
     return {"detail": "Backdrop deleted"} 
