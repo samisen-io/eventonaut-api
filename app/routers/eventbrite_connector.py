e3959c34d46f85bbcd35e8291990e8338f9e3ef6
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2024-04-25 13:08:41 +0530

    Refactor organization_crud.py and eventbrite_connector.py files

--- app/routers/eventbrite_connector.py
+++ app/routers/eventbrite_connector.py
@@ -12,7 +12,7 @@ from app.schemas.user_schemas import UserAuthentication as User
 from app.schemas import organization_settings_schemas as os_schemas
 from app.schemas.user_schemas import UserAuthentication as User
 from app.dependencies import get_db
-from app.crud.organization_crud import get_organization_by_id
+from app.crud.organization_crud import get_organization_by_id, get_organization_by_user_id, get_organization_by_uuid
 from app.static_enums.role import RoleEnum
 
 router = APIRouter(tags=["Eventbrite Connector"])
@@ -39,35 +39,38 @@ def sync_eventbrite_events( organization_id: str, private_token: str, eventbrite
     }
     response = requests.get(url, headers=headers)
     # Validate the response
-    organization = get_organization_by_id(db, organization_id)
-    if organization is None:
-        raise HTTPException(status_code=400, detail="Organization not found in Command Center. Please check your organization ID.")
-    if response.status_code != 200:
-        raise HTTPException(status_code=400, detail="Incorrect request. Please check your private token and organization ID.")
-    response = response.json()
-    owner_id = users_crud.get_user(db, current_user.id)
-    if not owner_id:
-        raise HTTPException(status_code=400, detail="User not found.")
-    owner_id = owner_id.id
-    # Save it in the database
-    organization_settings = {
-        'organization_id': organization_id,
-        'event_brite_org_id': eventbrite_organization_id,
-        'event_brite_access_token': private_token
-        }
-    org_settings = os_schemas.OrganizationSettingsCreate(**organization_settings)
-    create_organization_settings(db, org_settings, organization_id)
-    # Create webhooks for each event
-    for event in response["events"]:
-        event_id = event["id"] 
-        # create venue
-        event_venue = get_eventbrite_venue(event_id, private_token)
-        event_venue = add_venue(db,event_venue,owner_id)
-        event_venue_id = event_venue.id
-        #add event
-        event = add_event(db,event,owner_id,event_venue_id)
-        create_webhook(event_id, private_token, eventbrite_organization_id)
-        # Save the event details in the database
+    print(current_user.id)
+    # organization = get_organization_by_uuid(db, organization_id)
+    organization = get_organization_by_user_id(db, current_user.id)
+    print(organization.uuid)
+    # if organization is None:
+    #     raise HTTPException(status_code=400, detail="Organization not found in Command Center. Please check your organization ID.")
+    # if response.status_code != 200:
+    #     raise HTTPException(status_code=400, detail="Incorrect request. Please check your private token and organization ID.")
+    # response = response.json()
+    # owner_id = users_crud.get_user(db, current_user.id)
+    # if not owner_id:
+    #     raise HTTPException(status_code=400, detail="User not found.")
+    # owner_id = owner_id.id
+    # # Save it in the database
+    # organization_settings = {
+    #     'organization_id': organization_id,
+    #     'event_brite_org_id': eventbrite_organization_id,
+    #     'event_brite_access_token': private_token
+    #     }
+    # org_settings = os_schemas.OrganizationSettingsCreate(**organization_settings)
+    # create_organization_settings(db, org_settings, organization_id)
+    # # Create webhooks for each event
+    # for event in response["events"]:
+    #     event_id = event["id"] 
+    #     # create venue
+    #     event_venue = get_eventbrite_venue(event_id, private_token)
+    #     event_venue = add_venue(db,event_venue,owner_id)
+    #     event_venue_id = event_venue.id
+    #     #add event
+    #     event = add_event(db,event,owner_id,event_venue_id)
+    #     create_webhook(event_id, private_token, eventbrite_organization_id)
+    #     # Save the event details in the database
     return {"message": "Eventbrite events retrieved successfully."}
     
 @router.get('/get_webhooks/')
