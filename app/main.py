e40eced719a86111223bd586eefbd6a8c9083284
praveen1621 <praveen@samisen.io>
2024-04-30 13:17:53 +0530

    Refactor main.py to include exhibitor_documents router

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from .routers import signup_organizer
 from .routers import backdrop_gallery, eventbrite_connector, organization, organization_user, photo_booth, role
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, attendee, agenda
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents, organization_settings, exhibitors, exhibitor_documents
+from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents, organization_settings, exhibitors, attendee_exhibitors, exhibitor_documents
 from .crud import logout_token_crud
 
 
@@ -82,6 +82,7 @@ app.include_router(photo_booth.router)
 app.include_router(attendee.router)
 app.include_router(attendee_conference.router)
 app.include_router(agenda.router)
+app.include_router(attendee_exhibitors.router)
 app.include_router(assistant.router)
 app.include_router(otp.router)
 app.include_router(authentication.router)
