87417d27fe23a67c059ceafe6cc8b0a1fb933225
praveen1621 <142780490+praveen1621@users.noreply.github.com>
2024-05-08 18:10:00 +0530

    Refactor API endpoints to use OrganizationSecurity (#294)
    * Refactor event_documents.py to use OrganizationSecurity instead of User in relevant functions
    
    * feat: Add email validation for exhibitor contact email
    
    * Refactor session_documents.py to use OrganizationSecurity instead of User in relevant functions

--- app/routers/session_documents.py
+++ app/routers/session_documents.py
@@ -9,17 +9,16 @@ from ..crud import sessions_crud
 from app.schemas.user_schemas import UserAuthentication as User
 from ..crud import session_document_crud as crud
 from ..schemas.session_document_schemas import SessionDocumentResponse, SessionDocumentRequest
-from ..static_enums.blob_container_enums import BlobContainer
+from ..schemas.organization_schemas import OrganizationSecurity
 import logging
-from app.static_enums.role import RoleEnum
 from app.static_enums.role import RoleEnum
 
 router = APIRouter(tags=["session_documents"], prefix="/session_documents")
 
 @router.post("/", response_model=SessionDocumentResponse, status_code=status.HTTP_201_CREATED)
-def create_session_document(session_id: str, file: UploadFile = File(...), db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name,"organizer"])):
+def create_session_document(session_id: str, file: UploadFile = File(...), db: Session = Depends(get_db), current_organization: OrganizationSecurity = Security(get_current_active_organization, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name,"organizer"])):
     try:
-        session = sessions_crud.get_session_by_uuid_id(db, session_id, current_user.id)
+        session = sessions_crud.get_session_by_uuid_id(db, session_id, current_organization.id)
         if not session:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Session not found")
         
@@ -53,8 +52,8 @@ def map_session_document_response(session_id, response):
                                      size=f"{str(response.size)} MB")
     
 @router.get("/", response_model=list[SessionDocumentResponse])
-def get_all_session_documents_by_organization(offset: int = 0, limit: int = 100,db: Session = Depends(get_db), organization: models.Organization = Security(get_current_active_organization, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name])):
-    documents = crud.get_all_session_documents_by_organization(db, organization.id, offset, limit)
+def get_all_session_documents_by_organization(offset: int = 0, limit: int = 100,db: Session = Depends(get_db), current_organization: OrganizationSecurity = Security(get_current_active_organization, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name])):
+    documents = crud.get_all_session_documents_by_organization(db, current_organization.id, offset, limit)
     if len(documents) == 0:
         logging.exception("No documents found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No documents found")
@@ -62,10 +61,12 @@ def get_all_session_documents_by_organization(offset: int = 0, limit: int = 100,
 
 @router.get("/{session_id}", response_model=list[SessionDocumentResponse])
 def get_all_session_documents(session_id: str, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name, RoleEnum.ATTENDEE.name, "organizer", "attendee"])):
-    if current_user.role == "organizer":
-        session = sessions_crud.get_session_by_uuid_id(db, session_id, current_user.id)
-    elif current_user.role == "attendee":
+    roles = [user_role.role for user_role in current_user.user_roles]
+    if roles[0].name == RoleEnum.ATTENDEE.name:
         session = sessions_crud.get_session_by_session_uuid(db, session_id)
+    elif roles[0].name == RoleEnum.ORGANIZATION_ADMIN.name or roles[0].name == RoleEnum.ORGANIZATION_USER.name:
+        organization_id = current_user.organization_user[0].organization_id
+        session = sessions_crud.get_session_by_uuid_id(db, session_id, organization_id)
     if not session:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Session not found")
     documents = crud.get_session_documents_by_session_id(db, session.id)
