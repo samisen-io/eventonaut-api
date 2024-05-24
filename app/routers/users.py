766d082a65e526b61eaad702b896ae9f613462b7
praveen1621 <praveen@samisen.io>
2024-05-24 15:37:48 +0530

    Refactor update_user function to use consistent parameter naming and remove unnecessary code

--- app/routers/users.py
+++ app/routers/users.py
@@ -150,7 +150,7 @@ def get_user(db: Session = Depends(get_db), current_user:  User = Security(get_c
     return db_user
 
 @router.put("/", response_model=schemas.User)
-def update_user_(user: schemas.UserBaseUpdate, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name, "organizer"])):
+def update_user(user: schemas.UserBaseUpdate, db: Session = Depends(get_db), current_user: User = Security(get_current_active_user, scopes=[RoleEnum.ORGANIZATION_ADMIN.name, RoleEnum.ORGANIZATION_USER.name, "organizer"])):
     if user.profile_image_url is not None:
         user.profile_image_url = crud.validate_image_url(user.profile_image_url)
     db_user = crud.get_db_user(db, user_id=current_user.id)
