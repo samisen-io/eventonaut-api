d41ba5ef25ed742d5a88c5ac4ff5f49e56089e97
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2024-05-24 19:53:17 +0530

    Refactor report generation operations and routers for improved functionality and code organization

--- app/report_generation_operations.py
+++ app/report_generation_operations.py
@@ -4,8 +4,16 @@ from fastapi import HTTPException
 import requests
 import pypugjs.ext.jinja
 from jinja2 import Environment, FileSystemLoader
-from weasyprint import HTML
 from io import BytesIO
+import pdfkit
+# os.environ['WEASYPRINT_DLL_DIRECTORIES']=r'C:\Program Files\GTK3-Runtime Win64\bin'
+# os.environ['WEASYPRINT_DLL_DIRECTORIES']=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "GTK3-Runtime Win64", "bin")
+# print(os.environ['WEASYPRINT_DLL_DIRECTORIES'])
+# from weasyprint import HTML
+
+path_wkhtmltopdf = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),"wkhtmltopdf", "wkhtmltopdf.exe")
+config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
 
 def render_pug_template(template_path, context):
     template_dir, template_name = os.path.split(template_path)
@@ -21,7 +29,8 @@ def generate_report_using_template(template, input_data):
     html = render_pug_template(temp_file.name, input_data)
     pdf_io = BytesIO()
     try:
-        HTML(string=html).write_pdf(pdf_io)
+        pdf = pdfkit.from_string(html, False)
+        pdf_io.write(pdf)
         if pdf_io.tell() > 0:
             pdf_io.seek(0)
             os.remove(temp_file.name)
