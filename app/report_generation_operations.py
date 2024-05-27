7a4497f3e653943e32b0ed426a3e82d54fe4baad
Abhaylmg <142780053+Abhaylmg@users.noreply.github.com>
2024-05-27 08:31:36 +0530

    Refactor routers and add report generation functionality (#322)
    * Refactor routers to include reports functionality
    
    * Refactor report generation operations and routers for improved functionality and code organization
    
    * Refactor report generation operations and routers for improved functionality and code organization
    
    * Refactor report generation operations and routers for improved functionality and code organization
    
    * Refactor report generation operations and routers for improved functionality and code organization
    
    * Refactor Dockerfile to install additional dependencies for improved functionality
    
    * Removed unnecessary DLL and EXE files from GTK3-Runtime and wkhtmltopdf directories
    
    ---------
    
    Co-authored-by: praveen1621 <praveen@samisen.io>

---
+++ app/report_generation_operations.py
@@ -0,0 +1,35 @@
+import os
+import tempfile
+from fastapi import HTTPException
+import requests
+import pypugjs.ext.jinja
+from jinja2 import Environment, FileSystemLoader
+from io import BytesIO
+from weasyprint import HTML
+
+
+def render_pug_template(template_path, context):
+    template_dir, template_name = os.path.split(template_path)
+    env = Environment(loader = FileSystemLoader(template_dir), extensions=['pypugjs.ext.jinja.PyPugJSExtension'])
+    template = env.get_template(template_name)
+    return template.render(context)
+
+def generate_report_using_template(template, input_data):
+    template_url = template.template_url
+    response = requests.get(template_url)
+    with tempfile.NamedTemporaryFile(mode = 'w', encoding='utf-8', delete=False) as temp_file:
+        temp_file.write(response.content.decode('utf-8'))
+    html = render_pug_template(temp_file.name, input_data)
+    pdf_io = BytesIO()
+    try:
+        pdf = HTML(string=html).write_pdf()
+        pdf_io.write(pdf)
+        if pdf_io.tell() > 0:
+            pdf_io.seek(0)
+            os.remove(temp_file.name)
+            return pdf_io
+        else:
+            raise HTTPException(status_code=500, detail='Error generating report')
+    except Exception as e:
+        os.remove(temp_file.name)
+        raise HTTPException(status_code=500, detail='Error generating report')
