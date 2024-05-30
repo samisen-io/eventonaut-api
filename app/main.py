acecba935093bc330260f3b1e16d0b41d97c648d
praveen1621 <praveen@samisen.io>
2024-05-30 11:44:16 +0530

    Refactor main.py to include attendee check-in functionality

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from .routers import signup_organizer
 from .routers import backdrop_gallery, eventbrite_connector, organization, organization_user, photo_booth, role
 from .routers import ai_models, users, conferences, ai_models, sessions, settings, attendee, agenda
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents, organization_settings, exhibitors, attendee_exhibitors, exhibitor_documents, template
+from .routers import ai_models, users, conferences, ai_models, sessions, settings, authentication, otp, assistant, attendee_conference, client, speakers, promotions,sponsor, venue, static_organizer, static_client, static_event, static_session, static_attendee, upload_image, event_documents, session_documents, organization_settings, exhibitors, attendee_exhibitors, exhibitor_documents, template, attendee_checkin
 from .crud import logout_token_crud
 
 
@@ -77,6 +77,7 @@ app.include_router(settings.router)
 # app.include_router(ai_models.router, dependencies=[Depends(get_current_active_user)])
 app.include_router(ai_models.router)
 app.include_router(photo_booth.router)
+app.include_router(attendee_checkin.router)
 app.include_router(attendee.router)
 app.include_router(attendee_conference.router)
 app.include_router(agenda.router)
