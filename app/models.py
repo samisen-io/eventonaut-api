3507cbbe6641c9b0f9b25095e8522a104f006f56
praveen1621 <praveen@samisen.io>
2024-06-20 00:00:21 +0530

    Refactor order_id column in RegistrationOrder model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import UniqueConstraint
 from .database import Base
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy import text
 
 class Organization(Base):
     __tablename__ = "organization"
