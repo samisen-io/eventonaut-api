98dbb1a0d4987d53f00f947b5b5359c9ccd4fa3e
praveen1621 <praveen@samisen.io>
2024-07-01 09:49:40 +0530

    feat: Add functionality to send tickets via email
    This commit adds the `send_tickets` function to the `otp_generator.py` file. The function allows sending tickets to a specified email address as an attachment. It uses the `download_ticket` function from the `upload_image` module to retrieve the ticket and attaches it to the email. This feature enhances the user experience by providing a convenient way to send tickets to attendees.

--- app/otp_generator.py
+++ app/otp_generator.py
@@ -1,3 +1,4 @@
+from email.mime.application import MIMEApplication
 import random
 import logging
 import string
@@ -7,6 +8,7 @@ from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 import os
 from .email_templates.welcome_user import WelcomeUserEnum
+from .routers.upload_image import download_ticket
 
 load_dotenv()
 
@@ -75,3 +77,47 @@ def send_mail(unique_id: str, subject: str, receiver_email:str, first_name:str, 
         except Exception as e:
             logging.exception(str(e))
             return False
+        
+def send_tickets(receiver_email: str, blob_url: str, subject: str):
+    try:
+        global email, password, command_center_link
+        smtp_port = 587
+        smtp_server = "smtp.gmail.com"
+        sender_email = email
+        password = password
+        
+        ticket = download_ticket(blob_url)
+        
+        logging.info("Connecting to server")
+        server = smtplib.SMTP(smtp_server, smtp_port)
+
+        server.starttls()
+
+        server.login(sender_email, password)
+        logging.info("Login successful")
+
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+        msg['Subject'] = subject
+
+        # Message body
+        body = "Please find attached your tickets."
+        msg.attach(MIMEText(body, 'plain'))
+
+        # PDF attachment from memory
+        part = MIMEApplication(ticket, Name="Ticket.pdf")
+        part['Content-Disposition'] = 'attachment; filename="Ticket.pdf"'
+        msg.attach(part)
+
+        server.send_message(msg)
+        logging.info(f"Email sent to {receiver_email}")
+
+    except Exception as e:
+        logging.exception(f"failing to send email to {receiver_email} due to {str(e)}")
+        return False
+    finally:
+        server.quit()
+        logging.info("Server closed")
+
+    return True
